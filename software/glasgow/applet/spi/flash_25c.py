d50c7547ab32df179cb63273d835b8d8c262d607
whitequark <whitequark@whitequark.org>
2018-09-04 05:46:29 +0000

    applet.spi.flash_25c: add basic erase and programming support.

--- software/glasgow/applet/spi/flash_25c.py
+++ software/glasgow/applet/spi/flash_25c.py
@@ -52,7 +52,7 @@ class SPIFlash25CInterface:
         return (manufacturer_id, device_id)
 
     def _format_addr(self, addr):
-        return [(addr >> 16) & 0xff, (addr >> 8) & 0xff, addr & 0xff]
+        return bytes([(addr >> 16) & 0xff, (addr >> 8) & 0xff, addr & 0xff])
 
     async def _read_command(self, address, length, chunk_size, cmd, dummy=0):
         if chunk_size is None:
@@ -77,6 +77,46 @@ class SPIFlash25CInterface:
         self._log("fast read addr=%#08x len=%d", address, length)
         return await self._read_command(address, length, chunk_size, cmd=0x0B, dummy=1)
 
+    async def read_status(self):
+        status, = await self._command(0x05, ret=1)
+        self._log("read status=%s", "{:#010b}".format(status))
+        return status
+
+    async def write_status(self, status):
+        self._log("write status=%s", "{:#010b}".format(status))
+        await self._command(0x01, arg=[status])
+
+    async def write_enable(self):
+        self._log("write enable")
+        await self._command(0x06)
+
+    async def write_disable(self):
+        self._log("write disable")
+        await self._command(0x04)
+
+    async def write_in_progress(self):
+        return bool((await self.read_status()) & 1)
+
+    async def sector_erase(self, address):
+        self._log("sector erase addr=%#08x", address)
+        await self._command(0x20, arg=self._format_addr(address))
+        while await self.write_in_progress(): pass
+
+    async def page_program(self, address, data):
+        data = bytes(data)
+        self._log("page program addr=%#08x data=<%s>", address, data.hex())
+        await self._command(0x02, arg=self._format_addr(address) + data)
+        while await self.write_in_progress(): pass
+
+    async def program(self, address, data, page_size):
+        data = bytes(data)
+        while len(data) > 0:
+            chunk    = data[:page_size - address % page_size]
+            await self.write_enable()
+            await self.page_program(address, chunk)
+            data     = data[len(chunk):]
+            address += len(chunk)
+
 
 class SPIFlash25CApplet(SPIMasterApplet, name="spi-flash-25c"):
     logger = logging.getLogger(__name__)
@@ -91,6 +131,11 @@ class SPIFlash25CApplet(SPIMasterApplet, name="spi-flash-25c"):
 
     @classmethod
     def add_interact_arguments(cls, parser):
+        def address(arg):
+            return int(arg, 0)
+        def hex_bytes(arg):
+            return bytes.fromhex(arg)
+
         p_operation = parser.add_subparsers(dest="operation", metavar="OPERATION")
 
         p_identify = p_operation.add_parser(
@@ -98,7 +143,7 @@ class SPIFlash25CApplet(SPIMasterApplet, name="spi-flash-25c"):
 
         def add_read_arguments(parser):
             parser.add_argument(
-                "address", metavar="ADDRESS", type=int, default=0,
+                "address", metavar="ADDRESS", type=address, default=0,
                 help="read memory starting at address ADDRESS, with wraparound")
             parser.add_argument(
                 "length", metavar="LENGTH", type=int, default=0,
@@ -108,12 +153,44 @@ class SPIFlash25CApplet(SPIMasterApplet, name="spi-flash-25c"):
                 help="write memory contents to FILENAME")
 
         p_read = p_operation.add_parser(
-            "read", help="read memory using FAST READ command")
+            "read", help="read memory using READ command")
         add_read_arguments(p_read)
 
         p_fast_read = p_operation.add_parser(
             "fast-read", help="read memory using FAST READ command")
         add_read_arguments(p_fast_read)
+
+        def add_program_arguments(parser):
+            parser.add_argument(
+                "address", metavar="ADDRESS", type=address, default=0,
+                help="program memory starting at address ADDRESS")
+            g_data = parser.add_mutually_exclusive_group(required=True)
+            g_data.add_argument(
+                "-d", "--data", metavar="DATA", type=hex_bytes,
+                help="program memory with DATA as hex bytes")
+            g_data.add_argument(
+                "-f", "--file", metavar="FILENAME", type=argparse.FileType("wb"),
+                help="program memory with contents of FILENAME")
+
+        p_program_page = p_operation.add_parser(
+            "program-page", help="program memory page using PAGE PROGRAM command")
+        add_program_arguments(p_program_page)
+
+        p_program = p_operation.add_parser(
+            "program", help="program memory region using PAGE PROGRAM command")
+        p_program.add_argument(
+            "-P", "--page-size", metavar="SIZE", type=int, required=True,
+            help="program memory region using SIZE byte pages")
+        add_program_arguments(p_program)
+
+        def add_erase_arguments(parser, kind):
+            parser.add_argument(
+                "address", metavar="ADDRESS", type=address, default=0,
+                help="erase %s starting at address ADDRESS" % kind)
+
+        p_erase_sector = p_operation.add_parser(
+            "erase-sector", help="erase memory using SECTOR ERASE command")
+        add_erase_arguments(p_erase_sector, "sector")
 
     async def interact(self, device, args, flash_iface):
         await flash_iface.wakeup()
@@ -140,6 +217,22 @@ class SPIFlash25CApplet(SPIMasterApplet, name="spi-flash-25c"):
                 args.file.write(data)
             else:
                 print(data.hex())
+
+        if args.operation in ("program-page", "program"):
+            if args.data is not None:
+                data = args.data
+            if args.file is not None:
+                data = args.file.read()
+
+            if args.operation == "program-page":
+                await flash_iface.write_enable()
+                await flash_iface.page_program(args.address, args.data)
+            if args.operation == "program":
+                await flash_iface.program(args.address, args.data, args.page_size)
+
+        if args.operation == "erase-sector":
+            await flash_iface.write_enable()
+            await flash_iface.sector_erase(args.address)
 
 # -------------------------------------------------------------------------------------------------
 
