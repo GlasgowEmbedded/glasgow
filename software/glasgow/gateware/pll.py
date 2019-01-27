bb9984b22057209bb0e607db5b74ec82bdead5ed
whitequark <whitequark@whitequark.org>
2019-01-27 00:08:39 +0000

    gateware.pll: adjust for nMigen.
    This doesn't include adjustments for gateware.platform. Fortunately,
    only a few applets use PLL, so this can be done later.

--- software/glasgow/gateware/pll.py
+++ software/glasgow/gateware/pll.py
@@ -1,24 +1,20 @@
 import logging
-from nmigen.compat.fhdl.specials import Special
+from nmigen import *
 
 
 __all__ = ["PLL"]
 
 
-class PLL(Special):
-    def __init__(self, f_in, f_out, odomain, idomain="sys", logger=None):
-        super().__init__()
+class PLL(Elaboratable):
+    def __init__(self, f_in, f_out, odomain, idomain="sync", logger=None):
         self.logger  = logger or logging.getLogger(__name__)
         self.f_in    = float(f_in)
         self.f_out   = float(f_out)
         self.odomain = odomain
         self.idomain = idomain
 
-    def rename_clock_domain(self, old, new):
-        if self.idomain == old:
-            self.idomain = new
-        if self.odomain == old:
-            self.odomain = new
+    def elaborate(self, platform):
+        if hasattr(platform, "get_pll"):
+            return platform.get_pll(self)
 
-    def list_clock_domains(self):
-        return {self.idomain, self.odomain}
+        raise NotImplementedError("No PLL support for platform")
