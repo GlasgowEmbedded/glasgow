a71495229cbaf5bbb5a0a0927636d6cf1c1404bf
whitequark <whitequark@whitequark.org>
2019-02-06 01:06:41 +0000

    platform: remove IOStandard directives. NFC.
    They don't do anything on iCE40.

--- software/glasgow/platform/__init__.py
+++ software/glasgow/platform/__init__.py
@@ -8,8 +8,8 @@ __all__ = ['Platform']
 
 
 _io = [
-    ("clk_fx", 0, Pins("44"), IOStandard("LVCMOS33")),
-    ("clk_if", 0, Pins("20"), IOStandard("LVCMOS33")),
+    ("clk_fx", 0, Pins("44")),
+    ("clk_if", 0, Pins("20")),
 
     ("fx2", 0,
         Subsignal("sloe", Pins("6")),
@@ -19,35 +19,33 @@ _io = [
         Subsignal("fifoadr", Pins("4 3")),
         Subsignal("flag", Pins("11 10 9 48")),
         Subsignal("fd", Pins("19 18 17 16 15 14 13 12")),
-        IOStandard("LVCMOS33")
     ),
 
-    ("port_a", 0, Subsignal("io", Pins("45")), IOStandard("LVCMOS33")),
-    ("port_a", 1, Subsignal("io", Pins("43")), IOStandard("LVCMOS33")),
-    ("port_a", 2, Subsignal("io", Pins("42")), IOStandard("LVCMOS33")),
-    ("port_a", 3, Subsignal("io", Pins("38")), IOStandard("LVCMOS33")),
-    ("port_a", 4, Subsignal("io", Pins("37")), IOStandard("LVCMOS33")),
-    ("port_a", 5, Subsignal("io", Pins("36")), IOStandard("LVCMOS33")),
-    ("port_a", 6, Subsignal("io", Pins("35")), IOStandard("LVCMOS33")),
-    ("port_a", 7, Subsignal("io", Pins("34")), IOStandard("LVCMOS33")),
+    ("port_a", 0, Subsignal("io", Pins("45"))),
+    ("port_a", 1, Subsignal("io", Pins("43"))),
+    ("port_a", 2, Subsignal("io", Pins("42"))),
+    ("port_a", 3, Subsignal("io", Pins("38"))),
+    ("port_a", 4, Subsignal("io", Pins("37"))),
+    ("port_a", 5, Subsignal("io", Pins("36"))),
+    ("port_a", 6, Subsignal("io", Pins("35"))),
+    ("port_a", 7, Subsignal("io", Pins("34"))),
 
-    ("port_b", 0, Subsignal("io", Pins("32")), IOStandard("LVCMOS33")),
-    ("port_b", 1, Subsignal("io", Pins("31")), IOStandard("LVCMOS33")),
-    ("port_b", 2, Subsignal("io", Pins("28")), IOStandard("LVCMOS33")),
-    ("port_b", 3, Subsignal("io", Pins("27")), IOStandard("LVCMOS33")),
-    ("port_b", 4, Subsignal("io", Pins("26")), IOStandard("LVCMOS33")),
-    ("port_b", 5, Subsignal("io", Pins("25")), IOStandard("LVCMOS33")),
-    ("port_b", 6, Subsignal("io", Pins("23")), IOStandard("LVCMOS33")),
-    ("port_b", 7, Subsignal("io", Pins("21")), IOStandard("LVCMOS33")),
+    ("port_b", 0, Subsignal("io", Pins("32"))),
+    ("port_b", 1, Subsignal("io", Pins("31"))),
+    ("port_b", 2, Subsignal("io", Pins("28"))),
+    ("port_b", 3, Subsignal("io", Pins("27"))),
+    ("port_b", 4, Subsignal("io", Pins("26"))),
+    ("port_b", 5, Subsignal("io", Pins("25"))),
+    ("port_b", 6, Subsignal("io", Pins("23"))),
+    ("port_b", 7, Subsignal("io", Pins("21"))),
 
     ("i2c", 0,
         Subsignal("scl", Pins("39")),
         Subsignal("sda", Pins("40")),
-        IOStandard("LVCMOS33")
     ),
 
     # open-drain
-    ("sync", 0, Pins("41"), IOStandard("LVCMOS33")),
+    ("sync", 0, Pins("41")),
 ]
 
 _connectors = [
