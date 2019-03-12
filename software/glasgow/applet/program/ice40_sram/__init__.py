471d52bd7667ec35587bb2208cbce3ae265b4ae4
Sylvain Munaut <tnt@246tNt.com>
2019-03-12 14:05:07 +0100

    applet.program.ice40_sram: Rewrite to use the SPIMaster applet as base
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- software/glasgow/applet/program/ice40_sram/__init__.py
+++ software/glasgow/applet/program/ice40_sram/__init__.py
@@ -1,170 +1,116 @@
-import math
 import argparse
+import asyncio
 import logging
 from migen import *
 
+from ...interface.spi_master import SPIMasterApplet
 from ... import *
 
 
-class ProgramICE40SRAMSubtarget(Module):
-    def __init__(self, pads, out_fifo, sys_clk_freq):
-        oe = Signal()
-        self.comb += [
-            pads.rst_n_t.oe.eq(1),
-            pads.ss_n_t.oe.eq(oe),
-            pads.sck_t.oe.eq(oe),
-            pads.si_t.oe.eq(oe),
-        ]
+class ProgramICE40SRAMInterface:
+    def __init__(self, interface, logger, device, addr_dut_reset, addr_dut_done):
+        self.lower   = interface
+        self._logger = logger
+        self._level  = logging.DEBUG if self._logger.name == __name__ else logging.TRACE
+        self._device = device
+        self._addr_dut_reset = addr_dut_reset
+        self._addr_dut_done  = addr_dut_done
 
-        reset_cyc = math.ceil(1e-6 * sys_clk_freq)
-        start_cyc = math.ceil(1200e-6 * sys_clk_freq)
-        done_cyc  = 49
-        timer     = Signal(max=start_cyc)
+    async def set_reset(self, reset):
+        if self._addr_dut_reset is not None:
+            await self._device.write_register(self._addr_dut_reset, int(reset))
 
-        byteno = Signal(max=255)
-        bitno  = Signal(3)
-        shreg  = Signal(8)
+    async def get_done(self):
+        if self._addr_dut_reset is not None:
+            return await self._device.read_register(self._addr_dut_done)
 
-        divisor = Signal(4, reset=1)
+    async def program(self, bitstream):
+        # Pulse reset while setting SS_n low (we use a dummy write to do that)
+        await self.set_reset(True)
+        await self.lower.write([], hold_ss=True)
+        await self.lower.synchronize()
+        await self.set_reset(False)
 
-        self.submodules.fsm = FSM(reset_state="IDLE")
-        self.fsm.act("IDLE",
-            If(out_fifo.readable,
-                NextValue(oe, 1),
-                NextValue(pads.rst_n_t.o, 0),
-                NextValue(pads.sck_t.o, 1),
-                NextValue(timer, reset_cyc),
-                NextState("RESET")
-            ).Else(
-                NextValue(oe, 0),
-                NextValue(pads.rst_n_t.o, 1),
-            )
-        )
-        self.fsm.act("RESET",
-            If(timer == 0,
-                NextValue(pads.rst_n_t.o, 1),
-                NextValue(timer, start_cyc),
-                NextState("START")
-            ).Else(
-                NextValue(timer, timer - 1)
-            )
-        )
-        self.fsm.act("START",
-            If(timer == 0,
-                NextState("CHUNK")
-            ).Else(
-                NextValue(timer, timer - 1)
-            )
-        )
-        self.fsm.act("CHUNK",
-            If(out_fifo.readable,
-                out_fifo.re.eq(1),
-                If(out_fifo.dout == 0,
-                    NextValue(timer, done_cyc),
-                    NextState("DONE")
-                ).Else(
-                    NextValue(byteno, out_fifo.dout),
-                    NextState("LOAD")
-                )
-            )
-        )
-        self.fsm.act("LOAD",
-            If(out_fifo.readable,
-                out_fifo.re.eq(1),
-                NextValue(byteno, byteno - 1),
-                NextValue(bitno, 7),
-                NextValue(shreg, out_fifo.dout),
-                NextValue(divisor, divisor.reset),
-                NextState("SETUP")
-            )
-        )
-        self.fsm.act("SETUP",
-            NextValue(pads.sck_t.o, 0),
-            NextValue(pads.si_t.o, shreg[7]),
-            If(divisor == 0,
-                NextValue(divisor, divisor.reset),
-                NextState("HOLD")
-            ).Else(
-                NextValue(divisor, divisor - 1)
-            )
-        )
-        self.fsm.act("HOLD",
-            NextValue(pads.sck_t.o, 1),
-            If(divisor == 0,
-                NextValue(divisor, divisor.reset),
-                If((bitno == 0) & (byteno == 0),
-                    NextState("CHUNK")
-                ).Elif(bitno == 0,
-                    NextState("LOAD")
-                ).Else(
-                    NextValue(bitno, bitno - 1),
-                    NextValue(shreg, shreg << 1),
-                    NextState("SETUP")
-                )
-            ).Else(
-                NextValue(divisor, divisor - 1)
-            )
-        )
-        self.fsm.act("DONE",
-            NextValue(pads.sck_t.o, ~pads.sck_t.o),
-            If(pads.sck_t.o,
-                If(timer == 0,
-                    NextState("IDLE")
-                ).Else(
-                    NextValue(timer, timer - 1)
-                )
-            )
-        )
+        # Wait at least 1.2ms (spec for 8k devices)
+        await self.lower.delay_us(1200)
+
+        # Write bitstream
+        while len(bitstream) > 0:
+            chunk = bitstream[:255]
+            bitstream = bitstream[255:]
+            await self.lower.write(chunk, hold_ss=True)
+
+        # Specs says at least 49 dummy bits. Send 128.
+        await self.lower.write([0] * 16)
 
 
-class ProgramICE40SRAMApplet(GlasgowApplet, name="program-ice40-sram"):
-    preview = True
+class ProgramICE40SRAMApplet(SPIMasterApplet, name="program-ice40-sram"):
     logger = logging.getLogger(__name__)
     help = "program SRAM of iCE40 FPGAs"
     description = """
     Program the volatile bitstream memory of iCE40 FPGAs.
     """
-    pins = ("rst_n", "ss_n", "sck", "si")
 
     @classmethod
     def add_build_arguments(cls, parser, access):
-        access.add_build_arguments(parser)
-        for pin in cls.pins:
-            access.add_pin_argument(parser, pin, default=True)
+        super().add_build_arguments(parser, access, omit_pins=True)
+
+        access.add_pin_argument(parser, "sck",   required=True)
+        access.add_pin_argument(parser, "ss",    required=True)
+        access.add_pin_argument(parser, "mosi",  required=True)
+        access.add_pin_argument(parser, "reset", required=True)
+        access.add_pin_argument(parser, "done")
 
     def build(self, target, args):
-        self.mux_interface = iface = target.multiplexer.claim_interface(self, args)
-        iface.add_subtarget(ProgramICE40SRAMSubtarget(
-            pads=iface.get_pads(args, pins=self.pins),
-            out_fifo=iface.get_out_fifo(),
-            sys_clk_freq=target.sys_clk_freq,
-        ))
+        subtarget = super().build(target, args, pins=("sck", "ss", "mosi"))
+
+        if args.pin_reset is not None:
+            reset_t = self.mux_interface.get_pin(args.pin_reset)
+            dut_reset, self.__addr_dut_reset = target.registers.add_rw(1)
+            subtarget.comb += [
+                reset_t.o.eq(0),
+                reset_t.oe.eq(dut_reset),
+            ]
+        else:
+            self.__addr_dut_reset = None
+
+        if args.pin_done is not None:
+            done_t = self.mux_interface.get_pin(args.pin_done)
+            dut_done, self.__addr_dut_done = target.registers.add_ro(1)
+            subtarget.comb += [
+                dut_done.eq(done_t.i),
+            ]
+        else:
+            self.__addr_dut_done = None
+
+        return subtarget
 
     @classmethod
-    def add_run_arguments(cls, parser, access):
-        access.add_run_arguments(parser)
-
+    def add_interact_arguments(cls, parser):
         parser.add_argument(
             "bitstream", metavar="BITSTREAM", type=argparse.FileType("rb"),
             help="bitstream file")
 
     async def run(self, device, args):
-        iface = await device.demultiplexer.claim_interface(self, self.mux_interface, args)
+        spi_iface = await self.run_lower(ProgramICE40SRAMApplet, device, args)
+        return ProgramICE40SRAMInterface(spi_iface, self.logger, device,
+                                         self.__addr_dut_reset, self.__addr_dut_done)
 
+    async def interact(self, device, args, ice40_iface):
         bitstream = args.bitstream.read()
-        while len(bitstream) > 0:
-            chunk = bitstream[:255]
-            bitstream = bitstream[255:]
-            await iface.write([len(chunk)])
-            await iface.write(chunk)
-        await iface.write([0])
-        await iface.flush()
+        await ice40_iface.program(bitstream)
 
-        # TODO: do this nicely
-        # if args.port in device.poll_alert():
-        #     raise Exception("Port {} voltage went out of range during programming"
-        #                     .format(args.port))
-        # TODO: check CDONE
+        if (args.pin_done is not None) and (args.pin_reset is not None):
+            for _ in range(200):    # Wait up to 2s
+                await asyncio.sleep(0.010)  # Poll every 10 ms
+                done = await ice40_iface.get_done()
+                if done:
+                    break
+                if done:
+                    self.logger.info("FPGA successfully configured")
+                else:
+                    self.logger.warning("FPGA failed to configure after releasing reset")
+
 
 # -------------------------------------------------------------------------------------------------
 
