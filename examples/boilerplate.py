09e3fcd14c1b70dff35e0b48ce725719e1d701d1
whitequark <whitequark@whitequark.org>
2019-04-08 20:30:11 +0000

    examples: add boilerplate applet code to use as reference.

---
+++ examples/boilerplate.py
@@ -0,0 +1,65 @@
+import logging
+import asyncio
+from migen import *
+
+from ... import *
+
+
+class BoilerplateSubtarget(Module):
+    def __init__(self, pads, in_fifo, out_fifo):
+        pass
+
+
+class BoilerplateApplet(GlasgowApplet, name="boilerplate"):
+    logger = logging.getLogger(__name__)
+    help = "boilerplate applet"
+    preview = True
+    description = """
+    An example of the boilerplate code required to implement a minimal Glasgow applet.
+
+    The only things necessary for an applet are:
+        * a subtarget class,
+        * an applet class,
+        * the `build` and `run` methods of the applet class.
+
+    Everything else can be omitted and would be replaced by a placeholder implementation that does
+    nothing. Similarly, there is no requirement to use IN or OUT FIFOs, or any pins at all.
+    """
+
+    __pins = ()
+
+    @classmethod
+    def add_build_arguments(cls, parser, access):
+        super().add_build_arguments(parser, access)
+
+        for pin in cls.__pins:
+            access.add_pin_argument(parser, pin, default=True)
+
+    def build(self, target, args):
+        self.mux_interface = iface = target.multiplexer.claim_interface(self, args)
+        iface.add_subtarget(BoilerplateSubtarget(
+            pads=iface.get_pads(args, pins=self.__pins),
+            in_fifo=iface.get_in_fifo(),
+            out_fifo=iface.get_out_fifo(),
+        ))
+
+    @classmethod
+    def add_run_arguments(cls, parser, access):
+        super().add_run_arguments(parser, access)
+
+    async def run(self, device, args):
+        return await device.demultiplexer.claim_interface(self, self.mux_interface, args)
+
+    @classmethod
+    def add_interact_arguments(cls, parser):
+        pass
+
+    async def interact(self, device, args, iface):
+        pass
+
+# -------------------------------------------------------------------------------------------------
+
+class BoilerplateAppletTestCase(GlasgowAppletTestCase, applet=BoilerplateApplet):
+    @synthesis_test
+    def test_build(self):
+        self.assertBuilds()
