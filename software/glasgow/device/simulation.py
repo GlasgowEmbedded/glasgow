68664b5e2837c8ecad02b030eb085c114660c8ec
whitequark <whitequark@whitequark.org>
2019-04-14 12:31:22 +0000

    device.simulation: fix {read,write}_register APIs to match hardware.

--- software/glasgow/device/simulation.py
+++ software/glasgow/device/simulation.py
@@ -10,11 +10,11 @@ class GlasgowSimulationDevice:
         self._regs   = target.registers
 
     @asyncio.coroutine
-    def read_register(self, addr):
+    def read_register(self, addr, width=1):
         assert addr < self._regs.reg_count
         yield self._regs.regs_r[addr]
 
     @asyncio.coroutine
-    def write_register(self, addr, value):
+    def write_register(self, addr, value, width=1):
         assert addr < self._target.registers.reg_count
         yield self._regs.regs_w[addr].eq(value)
