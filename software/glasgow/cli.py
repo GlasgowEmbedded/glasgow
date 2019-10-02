GlasgowEmbedded/glasgow

commit 3ff1f20a8a11a1d09052d905fee8ddfdfdf66cac

cli, device.hardware: allow disambiguation by serial number.

software/glasgow/cli.py
@@ -9,7 +9,7 @@ import unittest
 from vcd import VCDWriter
 from datetime import datetime
 
-from fx2 import VID_CYPRESS, PID_FX2, FX2Config
+from fx2 import FX2Config
 from fx2.format import input_data, diff_data
 
 from ._version import get_versions
@@ -156,6 +156,24 @@ def get_argparser():
 
     parser = create_argparser()
 
+    def revision(arg):
+        revisions = ["A0", "B0", "C0", "C1"]
+        if arg in revisions:
+            return arg
+        else:
+            raise argparse.ArgumentTypeError("{} is not a valid revision (should be one of: {})"
+                                             .format(arg, ", ".join(revisions)))
+
+    def serial(arg):
+        if re.match(r"^[A-C][0-9]-\d{8}T\d{6}Z$", arg):
+            return arg
+        else:
+            raise argparse.ArgumentTypeError("{} is not a valid serial number".format(arg))
+
+    parser.add_argument(
+        "--serial", metavar="SERIAL", type=serial,
+        help="use device with serial number SERIAL")
+
     subparsers = parser.add_subparsers(dest="action", metavar="COMMAND")
     subparsers.required = True
 
@@ -254,20 +272,6 @@ def get_argparser():
         help="remove any bitstream present")
     add_applet_arg(g_flash_bitstream, mode="build")
 
-    def revision(arg):
-        revisions = ["A0", "B0", "C0", "C1"]
-        if arg in revisions:
-            return arg
-        else:
-            raise argparse.ArgumentTypeError("{} is not a valid revision (should be one of: {})"
-                                             .format(arg, ", ".join(revisions)))
-
-    def serial(arg):
-        if re.match(r"^\d{8}T\d{6}Z$", arg):
-            return arg
-        else:
-            raise argparse.ArgumentTypeError("{} is not a valid serial number".format(arg))
-
     p_build = subparsers.add_parser(
         "build", formatter_class=TextHelpFormatter,
         help="(advanced) build applet logic and save it as a file")
@@ -293,6 +297,12 @@ def get_argparser():
         help="(advanced) test applet logic without target hardware")
     add_applet_arg(p_test, mode="test", required=True)
 
+    def factory_serial(arg):
+        if re.match(r"^\d{8}T\d{6}Z$", arg):
+            return arg
+        else:
+            raise argparse.ArgumentTypeError("{} is not a valid serial number".format(arg))
+
     p_factory = subparsers.add_parser(
         "factory", formatter_class=TextHelpFormatter,
         help="(advanced) initial device programming")
@@ -300,10 +310,10 @@ def get_argparser():
         "--force", default=False, action="store_true",
         help="reinitialize the device, even if it is already programmed")
     p_factory.add_argument(
-        "--rev", metavar="REVISION", type=revision, required=True,
+        "--rev", metavar="REVISION", dest="factory_rev", type=revision, required=True,
         help="board revision")
     p_factory.add_argument(
-        "--serial", metavar="SERIAL", type=str,
+        "--serial", metavar="SERIAL", dest="factory_serial", type=factory_serial,
         default=datetime.now().strftime("%Y%m%dT%H%M%SZ"),
         help="serial number in ISO 8601 format (if not specified: %(default)s)")
 
@@ -404,14 +414,15 @@ async def _main():
     create_logger(args)
 
     try:
-        firmware_file = os.path.join(os.path.dirname(__file__), "glasgow.ihex")
+        # TODO(py3.7): use importlib.resources
+        firmware_filename = os.path.join(os.path.dirname(__file__), "glasgow.ihex")
         if args.action in ("build", "test", "tool"):
             pass
         elif args.action == "factory":
-            device = GlasgowHardwareDevice(firmware_file, VID_CYPRESS, PID_FX2,
-                                           _revision_override=args.rev)
+            device = GlasgowHardwareDevice(args.serial, firmware_filename,
+                                           _factory_rev=args.factory_rev)
         else:
-            device = GlasgowHardwareDevice(firmware_file)
+            device = GlasgowHardwareDevice(args.serial, firmware_filename)
 
         if args.action == "voltage":
             if args.voltage is not None:
@@ -719,7 +730,7 @@ async def _main():
             fx2_config = FX2Config(vendor_id=VID_QIHW, product_id=PID_GLASGOW,
                                    device_id=GlasgowConfig.encode_revision(args.rev),
                                    i2c_400khz=True)
-            glasgow_config = GlasgowConfig(args.rev, args.serial)
+            glasgow_config = GlasgowConfig(args.factory_rev, args.factory_serial)
             fx2_config.append(0x4000 - GlasgowConfig.size, glasgow_config.encode())
 
             image = fx2_config.encode()

software/glasgow/device/hardware.py
@@ -5,7 +5,7 @@ import logging
 import usb1
 import asyncio
 import threading
-from fx2 import REQ_RAM, REG_CPUCS
+from fx2 import VID_CYPRESS, PID_FX2, REQ_RAM, REG_CPUCS
 from fx2.format import input_data
 
 from ..support.logging import *
@@ -54,74 +54,87 @@ class _PollerThread(threading.Thread):
 
 
 class GlasgowHardwareDevice:
-    def _open_device(self, vendor_id, product_id):
-        try:
-            self.usb = self.usb_context.openByVendorIDAndProductID(vendor_id, product_id)
-        except usb1.USBErrorAccess:
-            raise GlasgowDeviceError("cannot access device {:04x}:{:04x}"
-                                     .format(vendor_id, product_id))
-        if self.usb is None:
-            raise GlasgowDeviceError("device {:04x}:{:04x} not found"
-                                     .format(vendor_id, product_id))
-
-        try:
-            self.usb.setAutoDetachKernelDriver(True)
-        except usb1.USBErrorNotSupported:
-            pass
-
-    def _write_ram(self, addr, data):
-        while len(data) > 0:
-            chunk_length = min(len(data), 4096)
-            self.usb.controlWrite(usb1.REQUEST_TYPE_VENDOR, REQ_RAM, addr, 0, data[:chunk_length])
-            addr += chunk_length
-            data = data[chunk_length:]
-
-    def _cpu_reset(self, is_reset):
-        self._write_ram(REG_CPUCS, [1 if is_reset else 0])
-
-    def _download_firmware(self, chunks):
-        self._cpu_reset(True)
-        for address, data in chunks:
-            self._write_ram(address, data)
-        self._cpu_reset(False)
-
-    def __init__(self, firmware_file=None, vendor_id=VID_QIHW, product_id=PID_GLASGOW,
-                 _revision_override=None):
+    def __init__(self, serial=None, firmware_filename=None, *, _factory_rev=None):
         self.usb_context = usb1.USBContext()
         self.usb_poller = _PollerThread(self.usb_context)
         self.usb_poller.start()
 
-        self._open_device(vendor_id, product_id)
+        firmware = None
+        handles  = {}
+        discover = True
+        while discover:
+            discover = False
 
-        device_id = self.usb.getDevice().getbcdDevice()
-        if _revision_override is None:
-            self.revision = GlasgowConfig.decode_revision(device_id & 0xFF)
+            for device in self.usb_context.getDeviceIterator():
+                vendor_id  = device.getVendorID()
+                product_id = device.getProductID()
+                device_id  = device.getbcdDevice()
+                if _factory_rev is None:
+                    if (vendor_id, product_id) != (VID_QIHW, PID_GLASGOW):
+                        continue
+                    revision = GlasgowConfig.decode_revision(device_id & 0xFF)
+                else:
+                    if (vendor_id, product_id) != (VID_CYPRESS, PID_FX2):
+                        continue
+                    revision = _factory_rev
+
+                if device_id & 0xFF00 in (0x0000, 0xA000):
+                    if firmware_filename is None:
+                        logger.warn("found device without firmware, but no firmware is provided")
+                        continue
+                    elif firmware is None:
+                        logger.debug("loading firmware from %s", firmware_filename)
+                        with open(firmware_filename, "rb") as f:
+                            firmware = input_data(f, fmt="ihex")
+
+                    logger.debug("loading firmware to rev%s device", revision)
+                    handle = device.open()
+                    handle.controlWrite(usb1.REQUEST_TYPE_VENDOR, REQ_RAM, REG_CPUCS, 0, [1])
+                    for address, data in firmware:
+                        while len(data) > 0:
+                            handle.controlWrite(usb1.REQUEST_TYPE_VENDOR, REQ_RAM,
+                                                  address, 0, data[:4096])
+                            data = data[4096:]
+                            address += 4096
+                    handle.controlWrite(usb1.REQUEST_TYPE_VENDOR, REQ_RAM, REG_CPUCS, 0, [0])
+                    handle.close()
+
+                    # And rediscover the device after it reenumerates.
+                    discover = True
+                else:
+                    handle = device.open()
+                    device_serial = handle.getASCIIStringDescriptor(
+                        device.getSerialNumberDescriptor())
+                    if device_serial in handles:
+                        continue
+
+                    logger.debug("found rev%s device with serial %s", revision, device_serial)
+                    handles[device_serial] = (revision, handle)
+
+            if discover:
+                # Give every device we loaded firmware onto a bit of time to reenumerate.
+                time.sleep(1.0)
+
+        if len(handles) == 0:
+            raise GlasgowDeviceError("device not found")
+        if serial is None:
+            if len(handles) > 1:
+                raise GlasgowDeviceError("found {} devices (serial numbers {}), but a serial "
+                                         "number is not specified"
+                                         .format(len(handles), ", ".join(handles.keys())))
         else:
-            self.revision = _revision_override
+            if serial not in handles:
+                raise GlasgowDeviceError("device with serial number {} not found"
+                                         .format(serial))
 
-        if device_id & 0xFF00 in (0x0000, 0xA000):
-            logger.debug("found rev%s device without firmware", self.revision)
-
-            if firmware_file is None:
-                raise GlasgowDeviceError("firmware is not uploaded")
-
-            logger.debug("loading firmware from %s", firmware_file)
-            with open(firmware_file, "rb") as f:
-                self._download_firmware(input_data(f, fmt="ihex"))
-
-            # let the device re-enumerate and re-acquire it
-            time.sleep(1)
-            self._open_device(VID_QIHW, PID_GLASGOW)
-
-            # still not the right firmware?
-            if self.usb.getDevice().getbcdDevice() & 0xFF00 in (0x0000, 0xA000):
-                raise GlasgowDeviceError("firmware upload failed")
-
-        # https://github.com/vpelletier/python-libusb1/issues/39
-        # serial = self.usb.getDevice().getSerialNumber()
-        serial = self.usb.getASCIIStringDescriptor(
-            self.usb.getDevice().device_descriptor.iSerialNumber)
-        logger.debug("found rev%s device with serial %s", self.revision, serial)
+        if serial is None:
+            self.revision, self.usb = next(iter(handles.values()))
+        else:
+            self.revision, self.usb = handles[serial]
+        try:
+            self.usb.setAutoDetachKernelDriver(True)
+        except usb1.USBErrorNotSupported:
+            pass
 
     async def _do_transfer(self, is_read, setup):
         transfer = self.usb.getTransfer()

commit bf829a639708fb02c0a7a9118821b0dad34222af

cli: fix `glasgow flash`.
Broken in 3ff1f20a.

software/glasgow/cli.py
@@ -672,8 +672,8 @@ async def _main():
                 new_image[0] = 0xC0 # see below
             else:
                 logger.info("using firmware from %r",
-                            args.firmware.name if args.firmware else firmware_file)
-                with (args.firmware or open(firmware_file, "rb")) as f:
+                            args.firmware.name if args.firmware else firmware_filename)
+                with (args.firmware or open(firmware_filename, "rb")) as f:
                     for (addr, chunk) in input_data(f, fmt="ihex"):
                         fx2_config.append(addr, chunk)
                 fx2_config.disconnect = True