GlasgowEmbedded/glasgow

commit 1b830bc3e7cec7f68c8409f54731ea5355a4e278

applet.debug.mips: restrict Rocc handshaking to EJTAG >2.0.
BMIPS seems to support it, but Lexra does not, even though both are
EJTAG 2.0, so disable it on EJTAG 2.0 completely.

Also, print every CONTROL bit, instead of only the overridden one.

software/glasgow/applet/debug/mips/__init__.py
@@ -58,15 +58,16 @@ class EJTAGDebugInterface(aobject, GDBRemote):
         self._log("read IMPCODE %s", self._impcode.bits_repr())
 
     async def _exchange_control(self, **fields):
-        field_desc = " ".join("{}={:b}".format(field, value)
-                              for field, value in fields.items())
-        self._log("write CONTROL %s", field_desc)
-
         control = self._control.copy()
-        control.Rocc  = 1
-        control.PrAcc = 1
+        if self._impcode.EJTAGver > 0:
+            # Some (but not all) EJTAG 1.x/2.0 cores implement Rocc handshaking. We ignore it,
+            # since there's no easy way to tell which one it is (on some Lexra cores, Rocc appears
+            # to be R/W, which breaks the handshaking mechanism.)
+            control.Rocc = 1
         for field, value in fields.items():
             setattr(control, field, value)
+
+        self._log("write CONTROL %s", control.bits_repr(omit_zero=True))
         control_bits = control.to_bits()
         await self.lower.write_ir(IR_CONTROL)
 
@@ -74,7 +75,7 @@ class EJTAGDebugInterface(aobject, GDBRemote):
         new_control = DR_CONTROL.from_bits(control_bits)
         self._log("read CONTROL %s", new_control.bits_repr(omit_zero=True))
 
-        if new_control.Rocc and control.Rocc:
+        if self._impcode.EJTAGver > 0 and control.Rocc and new_control.Rocc:
             raise EJTAGError("target has been unexpectedly reset")
 
         return new_control

commit 79fd7c57cccb2793065c08a33d6636033aa536b9

applet.debug.mips: don't reset PrAcc bit by accident.
This was broken in 1b830bc3, and caused most PrAcc instrutions to be
skipped.

software/glasgow/applet/debug/mips/__init__.py
@@ -59,6 +59,7 @@ class EJTAGDebugInterface(aobject, GDBRemote):
 
     async def _exchange_control(self, **fields):
         control = self._control.copy()
+        control.PrAcc = 1
         if self._impcode.EJTAGver > 0:
             # Some (but not all) EJTAG 1.x/2.0 cores implement Rocc handshaking. We ignore it,
             # since there's no easy way to tell which one it is (on some Lexra cores, Rocc appears