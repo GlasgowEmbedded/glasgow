b23acb46647a3d381daa77ab7897337b85fec028
Sam Littlewood <sam@thelittlewoods.co.uk>
2020-02-20 22:15:30 +0000

    Whitespace cleanup.

--- software/glasgow/applet/program/chipcon/__init__.py
+++ software/glasgow/applet/program/chipcon/__init__.py
@@ -15,30 +15,25 @@ class ProgramChipconApplet(GlasgowApplet, name="program-chipcon"):
     description = """
     Program and read back TI/Chipcon CC111x, CC251x and CC243x parts.
     """
-
     __pins = ( "dclk", "ddat", "resetn")
 
     @classmethod
     def add_build_arguments(cls, parser, access):
         super().add_build_arguments(parser, access)
-
         for pin in cls.__pins:
             access.add_pin_argument(parser, pin, default=True)
-
         parser.add_argument(
             "-f", "--frequency", metavar="FREQ", type=int, default=1000,
             help="set bit rate to FREQ kHz (default: %(default)s)")
 
     def build(self, target, args):
         self.mux_interface = iface = target.multiplexer.claim_interface(self, args)
-
         iface.add_subtarget(CCDPISubtarget(
             pads=iface.get_pads(args, pins=self.__pins),
             out_fifo=iface.get_out_fifo(),
             in_fifo=iface.get_in_fifo(auto_flush=False),
             period=math.ceil(target.sys_clk_freq / (args.frequency * 1000))
         ))
-
         # Connect up RESETN
         reset, self._addr_reset = target.registers.add_rw(1)
         target.comb += [
@@ -53,28 +48,20 @@ class ProgramChipconApplet(GlasgowApplet, name="program-chipcon"):
 
     @classmethod
     def add_interact_arguments(cls, parser):
-
         def address(arg):
             return int(arg, 0)
-
         def length(arg):
             return int(arg, 0)
-
         p_operation = parser.add_subparsers(dest="operation", metavar="OPERATION")
-
         p_identify = p_operation.add_parser(
             "identify", help="read identity and revision from connected device")
-
         p_erase = p_operation.add_parser(
             "erase", help="erase whole device.")
-
         p_erase_page = p_operation.add_parser(
             "erase-page", help="erase whole device.")
-
         p_erase_page.add_argument(
             "address", metavar="ADDRESS", type=address,
             help="erase memory from address ADDRESS")
-
         p_read = p_operation.add_parser(
             "read", help="read memory")
         p_read.add_argument(
@@ -89,7 +76,6 @@ class ProgramChipconApplet(GlasgowApplet, name="program-chipcon"):
         p_read.add_argument(
             "--lock-bits", metavar="LOCK-BITS", type=argparse.FileType("wb"),
             help="read flash information page into LOCK-BITS")
-
         p_write = p_operation.add_parser(
             "write", help="write and verify memory")
         p_write.add_argument(
@@ -113,27 +99,21 @@ class ProgramChipconApplet(GlasgowApplet, name="program-chipcon"):
             raise CCDPIError("cannot determine %s file format" % kind)
 
     async def interact(self, device, args, chipcon_iface):
-
         await chipcon_iface.connect()
         await chipcon_iface.clock_init()
-
         self.logger.info("connected to {} Rev:{}".format(
             chipcon_iface.device.name,
             chipcon_iface.chip_rev))
         self.logger.info(args.operation)
-
         if args.operation == "identify":
             self.logger.info("Id:{:X} [{}] Rev:{:d}".format(
                 chipcon_iface.chip_id,
                 chipcon_iface.device.name,
                 chipcon_iface.chip_rev))
-
         elif args.operation == "erase":
             await chipcon_iface.chip_erase()
-
         elif args.operation == "erase-page":
             await chipcon_iface.erase_flash_page(args,address)
-
         elif args.operation == "read":
             if args.code:
                 self._check_format(args.code, "code")
@@ -141,7 +121,6 @@ class ProgramChipconApplet(GlasgowApplet, name="program-chipcon"):
                 await chipcon_iface.set_config(0)
                 output_data(args.code,
                             await chipcon_iface.read_code(args.address, args.length))
-
             if args.lock_bits:
                 self._check_format(args.lock_bits, "lock-bits")
                 self.logger.info("reading flash information (%d bytes)", args.length)
@@ -149,12 +128,10 @@ class ProgramChipconApplet(GlasgowApplet, name="program-chipcon"):
                 output_data(args.lock_bits,
                             await chipcon_iface.read_code(args.address, args.length))
                 await chipcon_iface.set_config(0)
-
         elif args.operation == "write":
             if not args.no_erase:
                 self.logger.info("erasing chip")
                 await chipcon_iface.chip_erase()
-
             if args.code:
                 self._check_format(args.code, "code")
                 data = input_data(args.code)
@@ -163,18 +140,15 @@ class ProgramChipconApplet(GlasgowApplet, name="program-chipcon"):
                 await chipcon_iface.set_config(0)
                 await self._write_flash_blocks(chipcon_iface, data,
                                                chipcon_iface.device.write_block_size)
-
             if args.lock_bits:
                 self._check_format(args.lock_bits, "lock-bits")
                 data = input_data(args.lock_bits)
                 self.logger.info("writing flash information (%d bytes)",
                                  sum([len(chunk) for address, chunk in data]))
-
                 await chipcon_iface.set_config(Config.SEL_FLASH_INFO_PAGE)
                 await self._write_flash_blocks(chipcon_iface, data,
                                                chipcon_iface.device.write_block_size)
                 await chipcon_iface.set_config(0)
-
         await chipcon_iface.disconnect()
 
     async def _write_flash_blocks(self, chipcon_iface, data, block_size):
@@ -203,7 +177,6 @@ class ProgramChipconApplet(GlasgowApplet, name="program-chipcon"):
         return (start, data_flat)
 
 # -------------------------------------------------------------------------------------------------
-
 class ProgramChipconAppletTestCase(GlasgowAppletTestCase, applet=ProgramChipconApplet):
     @synthesis_test
     def test_build(self):
--- software/glasgow/applet/program/chipcon/ccdpi.py
+++ software/glasgow/applet/program/chipcon/ccdpi.py
@@ -22,7 +22,6 @@ class CCDPIBus(Elaboratable):
     def __init__(self, pads, period):
         self.pads = pads
         self.half_period = period//2
-
         self.di  = Signal(8)
         self.do  = Signal(8)
         self.bits = Signal(range(8))
@@ -32,44 +31,34 @@ class CCDPIBus(Elaboratable):
 
     def elaborate(self, platform):
         m = Module()
-
         dclk = Signal()
         oe   = Signal()
         o    = Signal()
         i    = Signal()
-
         m.d.comb += [
             self.pads.dclk_t.oe.eq(1),
             self.pads.dclk_t.o.eq(dclk),
-
             self.pads.ddat_t.oe.eq(oe),
             self.pads.ddat_t.o.eq(o),
-
             i.eq(self.pads.ddat_t.i),
         ]
-
         # Strobe every half-period
         timer    = Signal(range(self.half_period))
         stb      = Signal()
-
         with m.If(timer == 0):
             m.d.sync += timer.eq(self.half_period - 1),
         with m.Else():
             m.d.sync += timer.eq(timer - 1),
-
         m.d.comb += stb.eq(timer == 0)
 
         d   = Signal(8)
         cnt = Signal(range(8))
-
         m.d.comb += self.di.eq(d)
-
         with m.FSM():
             with m.State("READY"):
                 m.d.comb += [
                     self.rdy.eq(1),
                 ]
-
                 with m.If(self.ack):
                     m.d.sync += cnt.eq(self.bits)
                     with m.If(self.w):
@@ -79,7 +68,6 @@ class CCDPIBus(Elaboratable):
                     with m.Else():
                         m.d.sync += oe.eq(0),
                         m.next = "READ_RISE"
-
             with m.State("WRITE_RISE"):
                 with m.If(stb):
                     m.d.sync += [
@@ -88,7 +76,6 @@ class CCDPIBus(Elaboratable):
                         cnt.eq(cnt-1),
                     ]
                     m.next = "WRITE_FALL"
-
             with m.State("WRITE_FALL"):
                 with m.If(stb):
                     m.d.sync += [
@@ -99,7 +86,6 @@ class CCDPIBus(Elaboratable):
                         m.next = "DONE"
                     with m.Else():
                         m.next = "WRITE_RISE"
-
             with m.State("READ_RISE"):
                 with m.If(stb):
                     m.d.sync += [
@@ -107,7 +93,6 @@ class CCDPIBus(Elaboratable):
                         cnt.eq(cnt-1),
                     ]
                     m.next = "READ_FALL"
-
             with m.State("READ_FALL"):
                 with m.If(stb):
                     m.d.sync += [
@@ -118,53 +103,50 @@ class CCDPIBus(Elaboratable):
                         m.next = "DONE"
                     with m.Else():
                         m.next = "READ_RISE"
-
-
             with m.State("DONE"):
                 with m.If(stb):
                     m.d.sync += oe.eq(0)
                     m.next = "READY"
-
         return m
 
 class Cmd(IntEnum):
-	CHIP_ERASE    = 0b0001_0100
-	WR_CONFIG     = 0b0001_1101
-	RD_CONFIG     = 0b0010_0100
-	GET_PC        = 0b0010_1000
-	READ_STATUS   = 0b0011_0100
-	SET_HW_BRKPNT = 0b0011_1111
-	HALT          = 0b0100_0100
-	RESUME        = 0b0100_1100
-	DEBUG_INSTR   = 0b0101_0100
-	DEBUG_INSTR1  = 0b0101_0101
-	DEBUG_INSTR2  = 0b0101_0110
-	DEBUG_INSTR3  = 0b0101_0111
-	STEP_INSTR    = 0b0101_1100
-	STEP_REPLACE  = 0b0110_0100
-	GET_CHIP_ID   = 0b0110_1000
+    CHIP_ERASE    = 0b0001_0100
+    WR_CONFIG     = 0b0001_1101
+    RD_CONFIG     = 0b0010_0100
+    GET_PC        = 0b0010_1000
+    READ_STATUS   = 0b0011_0100
+    SET_HW_BRKPNT = 0b0011_1111
+    HALT          = 0b0100_0100
+    RESUME        = 0b0100_1100
+    DEBUG_INSTR   = 0b0101_0100
+    DEBUG_INSTR1  = 0b0101_0101
+    DEBUG_INSTR2  = 0b0101_0110
+    DEBUG_INSTR3  = 0b0101_0111
+    STEP_INSTR    = 0b0101_1100
+    STEP_REPLACE  = 0b0110_0100
+    GET_CHIP_ID   = 0b0110_1000
 
 class Config(IntFlag):
-	TIMERS_OFF          = 0b0000_1000
-	DMA_PAUSE           = 0b0000_0100
-	TIMER_SUSPEND       = 0b0000_0010
-	SEL_FLASH_INFO_PAGE = 0b0000_0001
+    TIMERS_OFF          = 0b0000_1000
+    DMA_PAUSE           = 0b0000_0100
+    TIMER_SUSPEND       = 0b0000_0010
+    SEL_FLASH_INFO_PAGE = 0b0000_0001
 
 class Status(IntFlag):
-	CHIP_ERASE_DONE   = 0b1000_0000
-	PCON_IDLE         = 0b0100_0000
-	CPU_HALTED        = 0b0010_0000
-	POWER_MODE_0      = 0b0001_0000
-	HALT_STATUS       = 0b0000_1000
-	DEBUG_LOCKED      = 0b0000_0100
-	OSCILLATOR_STABLE = 0b0000_0010
-	STACK_OVERFLOW    = 0b0000_0001
+    CHIP_ERASE_DONE   = 0b1000_0000
+    PCON_IDLE         = 0b0100_0000
+    CPU_HALTED        = 0b0010_0000
+    POWER_MODE_0      = 0b0001_0000
+    HALT_STATUS       = 0b0000_1000
+    DEBUG_LOCKED      = 0b0000_0100
+    OSCILLATOR_STABLE = 0b0000_0010
+    STACK_OVERFLOW    = 0b0000_0001
 
 class Operation(IntEnum):
-	COMMAND         = 0b00
-	DEBUG           = 0b01
-	COMMAND_DISCARD = 0b10
-	
+    COMMAND         = 0b00
+    DEBUG           = 0b01
+    COMMAND_DISCARD = 0b10
+
 class CCDPISubtarget(Elaboratable):
     def __init__(self, pads, out_fifo, in_fifo, period):
         self.pads = pads
@@ -175,12 +157,10 @@ class CCDPISubtarget(Elaboratable):
     def elaborate(self, platform):
         m = Module()
         m.submodules.bus = bus = CCDPIBus(self.pads, self.period)
-
         discard = Signal()
         op = Signal()
         count_out = Signal(3)
         count_in = Signal(3)
-
         with m.FSM():
             with m.State("READY"):
                 m.d.comb += self.in_fifo.flush.eq(1)
@@ -188,18 +168,15 @@ class CCDPISubtarget(Elaboratable):
                     m.d.comb += self.out_fifo.re.eq(1)
                     m.d.sync += Cat(count_in, count_out, op, discard).eq(self.out_fifo.dout)
                     m.next = "START"
-
             with m.State("START"):
                 with m.Switch(op):
                     with m.Case(Operation.COMMAND):
                         m.next = "OUT"
                     with m.Case(Operation.DEBUG):
                         m.next = "DEBUG"
-
             with m.State("OUT"):
                 with m.If(count_out == 0):
                     m.next = "IN"
-
                 with m.Elif(bus.rdy & self.out_fifo.readable):
                     m.d.comb += [
                         self.out_fifo.re.eq(1),
@@ -208,9 +185,7 @@ class CCDPISubtarget(Elaboratable):
                         bus.bits.eq(8),
                         bus.ack.eq(1),
                     ]
-
                     m.d.sync += count_out.eq(count_out-1)
-
             with m.State("IN"):
                 with m.If(count_in == 0):
                     m.next = "READY"
@@ -221,17 +196,14 @@ class CCDPISubtarget(Elaboratable):
                         bus.ack.eq(1),
                     ]
                     m.next = "IN_WRITE"
-
             with m.State("IN_WRITE"):
                 with m.If(bus.rdy & self.in_fifo.writable):
                     m.d.comb += [
                         self.in_fifo.we.eq(~discard),
                         self.in_fifo.din.eq(bus.di),
                     ]
-
                     m.d.sync += count_in.eq(count_in-1)
                     m.next = "IN"
-
             with m.State("DEBUG"):
                 with m.If(bus.rdy):
                     m.d.comb += [
@@ -242,7 +214,6 @@ class CCDPISubtarget(Elaboratable):
                         bus.ack.eq(1),
                     ]
                     m.next = "READY"
-
         return m
 
 class CCDPIInterface:
@@ -254,7 +225,6 @@ class CCDPIInterface:
         self._logger = logger
         self._level  = logging.DEBUG if self._logger.name == __name__ else logging.TRACE
         self._addr_reset = addr_reset
-
         self.connected = False
         self.chip_id = 0
         self.chip_rev = 0
@@ -266,7 +236,6 @@ class CCDPIInterface:
         """Send operation code then output bytes to fifo."""
         if not self.connected:
             raise CCDPIError("not connected")
-
         start_code = (op << 6) + (len(out_bytes) << 3) + num_bytes_in
         await self.lower.write([start_code] + out_bytes)
 
@@ -278,7 +247,6 @@ class CCDPIInterface:
         """Read back input bytes."""
         if not self.connected:
             raise CCDPIError("not connected")
-
         return await self.lower.read(num_bytes_in)
 
     async def _send_recv(self, op, out_bytes, num_bytes_in):
@@ -289,9 +257,7 @@ class CCDPIInterface:
     async def connect(self):
         """Reset device into debug mode."""
         self.connected = True
-
         await self.lower.reset()
-
         # Generate entry signal - two clocks while reset
         await self.lower.device.write_register(self._addr_reset, 1)
         await asyncio.sleep(0.01)
@@ -299,26 +265,22 @@ class CCDPIInterface:
         await self.lower.flush()
         await asyncio.sleep(0.01)
         await self.lower.device.write_register(self._addr_reset, 0)
-
         # Is there something recognizable attached?
         self.chip_id,self.chip_rev = await self.get_chip_id()
         self.device = devices.get(self.chip_id, None)
         if not self.device:
             raise CCDPIError("Did not find device")
-
         # Wait for stable clock
         for c in range(50):
             if await self.get_status() & Status.OSCILLATOR_STABLE:
                 break
         else:
             raise CCDPIError("Oscillator not stable")
-
         await self.halt()
 
     async def disconnect(self):
         """Reset device into normal operation."""
         self.connected = False
-
         await self.lower.device.write_register(self._addr_reset, 1)
         await asyncio.sleep(0.01)
         await self.lower.device.write_register(self._addr_reset, 0)
@@ -386,10 +348,8 @@ class CCDPIInterface:
 
     async def read_code(self, linear_address, count):
         """Read from CODE address space."""
-
         if (linear_address // 0x8000) != ((linear_address+count-1) // 0x8000):
             raise CCDPIError("reading across a bank boundary")
-
         if linear_address < 0x8000:
             bank = 0
             address = linear_address
@@ -397,10 +357,8 @@ class CCDPIInterface:
             # CC2430 banking
             bank = linear_address // 0x8000
             address = (linear_address % 0x8000) + 0x8000
-
         await self.debug_instr(0x75, 0xC7, (bank * 16) + 1)                     # MOV MEMCTR, (bank * 16) + 1
         await self.debug_instr(0x90, (address >> 8) & 0xff, address & 0xff)     # MOV DPTR, address
-
         # Read in chunk - send out a burst of read insns, then read back the replies
         data = bytearray()
         while count:
@@ -410,18 +368,13 @@ class CCDPIInterface:
                 await self.debug_instr(0xE4)                                    #   CLR A
                 await self.debug_instr(0x93, discard=False)                     #   MOVC A, @A+DPTR
                 await self.debug_instr(0xA3)                                    #   INC DPTR
-
             await self._flush()
-
             data += await self._recv(c)
-
         return data
 
     async def read_xdata(self, address, count):
         """Read from XDATA address space."""
-
         await self.debug_instr(0x90, (address >> 8) & 0xff, address & 0xff)     # MOV DPTR, address
-
         # Read in chunk - send out a burst of read insns, then read back the replies
         data = bytearray()
         while count:
@@ -430,18 +383,13 @@ class CCDPIInterface:
             for n in range(c):
                 await self.debug_instr(0xE0, discard=False)                     #   MOVC A, @A+DPTR
                 await self.debug_instr(0xA3)                                    #   INC DPTR
-
             await self._flush()
-
             data += await self._recv(c)
-
         return data
 
     async def write_xdata(self, address, data):
         """Write to XDATA address space."""
-
         await self.debug_instr(0x90, (address >> 8) & 0xff, address & 0xff) # MOV DPTR, address
-
         for b in data:
             await self.debug_instr(0x74, b)                                     #   MOV A,#imm8
             await self.debug_instr(0xF0)                                        #   MOV @DPTR,A
@@ -450,12 +398,10 @@ class CCDPIInterface:
 
     async def clock_init(self, internal=False):
         """Set up high speed clock (24Mhz Xtal or 12Mhz internal RC). """
-
         if internal:
             clkcon = 0xc1
         else:
             clkcon = 0x80
-
         await self.debug_instr(0x75, 0xC6, clkcon)                              # MOV CLKCON,#imm8
         for c in range(50):
             if await self.debug_instr_a(0xE5, 0xBE) & 0x40:                     #   MOV A, SLEEP
@@ -465,12 +411,9 @@ class CCDPIInterface:
 
     async def erase_flash_page(self, address):
         """Erase one page of flash memory."""
-
         if (address % self.device.flash_page_size) != 0:
             raise CCDPIError("Address is not page aligned")
-
         word_address = address // self.device.flash_word_size
-
         await self.debug_instr(0x75, 0xAD, (word_address >> 8) & 0x7f)          # MOV FADDRH, #imm8
         await self.debug_instr(0x75, 0xAC, 0)                                   # MOV FADDRH, #0
         await self.debug_instr(0x75, 0xAE, 0x01)                                # MOV FLC, #01h ; ERASE
@@ -486,35 +429,27 @@ class CCDPIInterface:
         Expects flash to be erased already.
         Pads data with 0xFF so that writes can be to byte boundaries.
         """
-
         # Word align start and end of data by padding with 0xff
         start_pad = address % self.device.flash_word_size
         if start_pad != 0:
             data = bytes([0xff]*start_pad) + data
             address -= start_pad
-
         end_pad = len(data) % self.device.flash_word_size
         if end_pad != 0:
             data =  data + bytes([0xff]*(self.device.flash_word_size-end_pad))
-
         if len(data) > self.device.flash_page_size:
             raise CCDPIError("Trying to write more than a page of data to flash.")
-
         if not data:
             return
-
         # Copy data into SRAM at 0xf000
         data_address = 0xf000
         await self.write_xdata(data_address, data)
-
         words_per_flash_page = self.device.flash_page_size // self.device.flash_word_size
         word_address = address // self.device.flash_word_size
         word_length = len(data) // self.device.flash_word_size
-
         # Counters for nested DJNZ loop
         count_l = word_length & 0xff
         count_h = ((word_length >> 8) & 0xff) + (1 if count_l != 0 else 0)
-
         # Code to run from RAM
         code = [
             0x75, 0xAD, (word_address >> 8) & 0x7f,                         #    MOV FADDRH, #imm8
@@ -534,15 +469,12 @@ class CCDPIInterface:
             0xDF, 0xEF,                                                     #    DJNZ R7, 1$
             0xA5                                                            #    HALT
         ]
-
         # Copy code into SRAM in next page
         code_address = 0xf000 + self.device.flash_page_size
         await self.write_xdata(code_address, code)
-
         # Start CPU - then wait for it to halt
         await self.set_pc(code_address)
         await self.resume()
-
         for c in range(50):
             if await self.get_status() & Status.CPU_HALTED:
                 break
