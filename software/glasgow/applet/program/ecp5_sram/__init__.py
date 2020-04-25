14f60b4012b54feb7c96bb99bcc8c82ba6eadcd8
Greg Davill <greg.davill@gmail.com>
2020-04-25 09:33:43 +0930

    applet.program.ecp5_sram: rework applet
    Moved jtag constants into arch.lattice.ecp5
    Moved device idodes into database.lattice.ecp5

--- software/glasgow/applet/program/ecp5_sram/__init__.py
+++ software/glasgow/applet/program/ecp5_sram/__init__.py
@@ -2,37 +2,16 @@ import argparse
 import asyncio
 import logging
 import struct
-from bitarray import bitarray
-from nmigen.compat import *
 
 from ....arch.jtag import *
+from ....arch.lattice.ecp5 import *
 from ....support.bits import *
 from ....support.logging import *
-from ....protocol.jtag_svf import *
 from ....database.jedec import *
+from ....database.lattice.ecp5 import *
 from ... import *
 from ...interface.jtag_probe import JTAGProbeApplet, JTAGProbeStateTransitionError
 
-READ_ID =             bits.from_int(0xE0,8)
-LSC_READ_STATUS =     bits.from_int(0x3C,8)
-ISC_ENABLE =          bits.from_int(0xC6,8)
-ISC_DISABLE =         bits.from_int(0x26,8)
-ISC_ERASE =           bits.from_int(0x0E,8)
-LSC_BITSTREAM_BURST = bits.from_int(0x7A,8)
-
-
-ECP5_FAMILY_IDCODES = {
-	0x21111043 : "LFE5U-12"   ,
-	0x41111043 : "LFE5U-25"   ,
-	0x41112043 : "LFE5U-45"   ,
-	0x41113043 : "LFE5U-85"   ,
-	0x01111043 : "LFE5UM-25"  ,
-	0x01112043 : "LFE5UM-45"  ,
-	0x01113043 : "LFE5UM-85"  ,
-	0x81111043 : "LFE5UM5G-25",
-	0x81112043 : "LFE5UM5G-45",
-	0x81113043 : "LFE5UM5G-85"
-}
 
 class ProgramECP5SRAMInterface:
     def __init__(self, interface, logger, frequency):
@@ -40,15 +19,22 @@ class ProgramECP5SRAMInterface:
         self.logger = logger
         self._level  = logging.DEBUG if self.logger.name == __name__ else logging.TRACE
 
-    async def read_IDCODE(self):
+    async def _read_IDCODE(self):
         await self.lower.test_reset()
-        await self.lower.write_ir(READ_ID)
+        await self.lower.write_ir(IR_READ_ID)
         raw_bits = await self.lower.read_dr(32)
         idcode = struct.unpack("<I", raw_bits.to_bytes())[0]
         return idcode
         
-    async def check_IDCODE(self):
-        idcode_value = await self.read_IDCODE()
+    async def _read_STATUS(self):
+        await self.lower.write_ir(IR_LSC_READ_STATUS)
+        raw_bits = await self.lower.read_dr(32)
+        status_value = struct.unpack("<I", raw_bits.to_bytes())[0]
+        status = LSC_Status.from_int(status_value)
+        return status
+
+    async def identify(self):
+        idcode_value = await self._read_IDCODE()
         idcode   = DR_IDCODE.from_int(idcode_value)
         mfg_name = jedec_mfg_name_from_bank_num(idcode.mfg_id >> 7,
                                                 idcode.mfg_id & 0x7f) or \
@@ -57,57 +43,16 @@ class ProgramECP5SRAMInterface:
                             idcode.mfg_id, mfg_name, idcode.part_id, idcode.version)
         # Decode to actual ECP5 devices 
         try:
-            device = ECP5_FAMILY_IDCODES[idcode_value]
+            device = devices_by_idcode[idcode_value]
             self.logger.info("Found Device: %s", device)
         except:
             self.logger.error("IDCODE 0x%08X does not mtach ECP5 device", idcode_value)
 
 
-
-    async def read_STATUS(self):
-        await self.lower.write_ir(LSC_READ_STATUS)
-        raw_bits = await self.lower.read_dr(32)
-        status = struct.unpack("<I", raw_bits.to_bytes())[0]
-        return status
-
-    async def check_STATUS(self):
-        status = await self.read_STATUS()
-        self.logger.info("Status Register: 0x%08X", status)
-        self.logger.debug("  Transparent Mode:   %s", {True : "Yes", False : "No"}[status & (1 << 0)] )
-        self.logger.debug("  Config Target:      %s", {True : "eFuse"   , False : "SRAM"}[(status & (7 << 1)) > 0])
-        self.logger.debug("  Read Enable:        %s", {True : "Readable", False : "Not Readable"}[status & (1 << 11) != 0] )
-        self.logger.debug("  Write Enable:       %s", {True : "Writable", False : "Not Writable"}[status & (1 << 10) != 0] )
-        self.logger.debug("  JTAG Active:        %s", {True: "Yes", False: "No"}[status & (1 << 4)  != 0] )
-        self.logger.debug("  PWD Protection:     %s", {True: "Yes", False: "No"}[status & (1 << 5)  != 0] )
-        self.logger.debug("  Decrypt Enable:     %s", {True: "Yes", False: "No"}[status & (1 << 7)  != 0] )
-        self.logger.debug("  DONE:               %s", {True: "Yes", False: "No"}[status & (1 << 8)  != 0] )
-        self.logger.debug("  ISC Enable:         %s", {True: "Yes", False: "No"}[status & (1 << 9)  != 0] )
-        self.logger.debug("  Busy Flag:          %s", {True: "Yes", False: "No"}[status & (1 << 12) != 0] )
-        self.logger.debug("  Fail Flag:          %s", {True: "Yes", False: "No"}[status & (1 << 13) != 0] )
-        self.logger.debug("  Feature OTP:        %s", {True: "Yes", False: "No"}[status & (1 << 14) != 0] )
-        self.logger.debug("  Decrypt Only:       %s", {True: "Yes", False: "No"}[status & (1 << 15) != 0] )
-        self.logger.debug("  PWD Enable:         %s", {True: "Yes", False: "No"}[status & (1 << 16) != 0] )
-        self.logger.debug("  Encrypt Preamble:   %s", {True: "Yes", False: "No"}[status & (1 << 20) != 0] )
-        self.logger.debug("  Std Preamble:       %s", {True: "Yes", False: "No"}[status & (1 << 21) != 0] )
-        self.logger.debug("  SPIm Fail 1:        %s", {True: "Yes", False: "No"}[status & (1 << 22) != 0] )
-        self.logger.debug("  Execution Error:    %s", {True: "Yes", False: "No"}[status & (1 << 26) != 0] )
-        self.logger.debug("  ID Error:           %s", {True: "Yes", False: "No"}[status & (1 << 27) != 0] )
-        self.logger.debug("  Invalid Command:    %s", {True: "Yes", False: "No"}[status & (1 << 28) != 0] )
-        self.logger.debug("  SED Error:          %s", {True: "Yes", False: "No"}[status & (1 << 29) != 0] )
-        self.logger.debug("  Bypass Mode:        %s", {True: "Yes", False: "No"}[status & (1 << 30) != 0] )
-        self.logger.debug("  Flow Through Mode:  %s", {True: "Yes", False: "No"}[status & (1 << 31) != 0] )
-		
-        bse_error = (status & (7 << 23)) >> 23
-        self.logger.debug("  Flow Through Mode:  %s",{
-                0b000: "No Error (0b000)",
-                0b001: "ID Error (0b001)",
-                0b010: "CMD Error - illegal command (0b010)",
-                0b011: "CRC Error (0b011)",
-                0b100: "PRMB Error - preamble error (0b100)",
-                0b101: "ABRT Error - configuration aborted by the user (0b101)",
-                0b110: "OVFL Error - data overflow error (0b110)",
-                0b111: "SDM Error - bitstream pass the size of SRAM array (0b111)",
-            }[bse_error])
+    async def check_STATUS(self, status):
+        self.logger.info("Status Register: 0x%08X", status.to_int())
+        self.logger.info(" %s", status)
+    
 
     def reverse(self, a,size):
         b = 0
@@ -117,13 +62,12 @@ class ProgramECP5SRAMInterface:
         return b
 
     async def program(self, bitstream):
-        await self.check_IDCODE()
-        await self.check_STATUS()
+        await self.identify()
 
         # perform programming
-        await self.lower.write_ir(ISC_ENABLE)
+        await self.lower.write_ir(IR_ISC_ENABLE)
         await self.lower.run_test_idle(10)
-        await self.lower.write_ir(LSC_BITSTREAM_BURST)
+        await self.lower.write_ir(IR_LSC_BITSTREAM_BURST)
 
         # Send entire bitstream data into DR,
         # Bytes are expected MSB by the ECP5, so need to be reversed
@@ -134,15 +78,19 @@ class ProgramECP5SRAMInterface:
         await self.lower.enter_update_dr()
         
         
-        await self.lower.write_ir(ISC_DISABLE)
+        await self.lower.write_ir(IR_ISC_DISABLE)
         await self.lower.run_test_idle(10)
 
         # Check status
         # Note ECP5 will not release until STATUS is read
-        await self.check_STATUS()
+        status = await self._read_STATUS()
+
+        if status.DONE:
+            self.logger.info("Configuration Done")
+        else:
+            self.check_STATUS(status)
+
         
-
-
 
 
 class ProgramECP5SRAMApplet(JTAGProbeApplet, name="program-ecp5-sram"):
---
+++ software/glasgow/arch/lattice/ecp5.py
@@ -0,0 +1,54 @@
+from ...support.bits import *
+from ...support.bitstruct import *
+from collections import namedtuple
+
+
+# IR Values
+IR_READ_ID             = bits("11100000")
+IR_LSC_READ_STATUS     = bits("00111100")
+IR_ISC_ENABLE          = bits("11000110")
+IR_ISC_DISABLE         = bits("00100110")
+IR_ISC_ERASE           = bits("00001110")
+IR_LSC_BITSTREAM_BURST = bits("01111010")
+
+LSC_Status = bitstruct("LSC_STATUS", 32, [
+    ("Transparent_Mode", 1),
+    ("Config_Target",    3),
+    ("JTAG_Active",      1),
+    ("PWD_Protection",   1),
+    (None,               1), # Not used
+    ("Decrypt_Enable",   1),
+    ("DONE",             1),
+    ("ISC_Enable",       1),
+    ("Write_Enable",     1),
+    ("Read_Enable",      1),
+    ("Busy_Flag",        1),
+    ("Fail_Flag",        1),
+    ("FEA_OTP",          1),
+    ("Decrypt_Only",     1),
+    ("PWD_Enable",       1),
+    (None,               3), # Not used
+    ("Encrypt_Preamble", 1),
+    ("Std_Preamble",     1),
+    ("SPIm_Fail_1",      1),
+    ("BSE_Error_Code",   3),
+    ("Execution_Error",  1),
+    ("ID_Error",         1),
+    ("Invalid_Command",  1),
+    ("SED_Error",        1),
+    ("Bypass_Mode",      1),
+    ("Flow_Through_Mode",1),
+])
+
+BSEErrorCode = namedtuple("BSEErrorCode", ("code","error","error_info"))
+
+bse_error_code = [
+    BSEErrorCode(0b000, "No Error",   None                                   ),
+    BSEErrorCode(0b001, "ID Error",   None                                   ),
+    BSEErrorCode(0b010, "CMD Error",  "illegal command"                      ),
+    BSEErrorCode(0b011, "CRC Error",  None                                   ),
+    BSEErrorCode(0b100, "PRMB Error", "preamble error"                       ),
+    BSEErrorCode(0b101, "ABRT Error", "configuration aborted by the user"    ),
+    BSEErrorCode(0b110, "OVFL Error", "data overflow error"                  ),
+    BSEErrorCode(0b111, "SDM Error",  "bitstream pass the size of SRAM array"),
+]
