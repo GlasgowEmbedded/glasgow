a098c73e466713ef219d7be07f012b9fc38f3f35
Attie Grande <attie.grande@argentum-systems.co.uk>
2020-10-11 03:17:43 +0100

    support.si_prefix: implement "real" to "scaled with si prefix" number conversion

---
+++ software/glasgow/support/si_prefix.py
@@ -0,0 +1,16 @@
+def num_to_si(num, long_prefix=False):
+    prefixes = [
+        (  3, 'G', 'Giga'  ),
+        (  2, 'M', 'Mega'  ),
+        (  1, 'k', 'Kilo'  ),
+        (  0, '',  ''      ),
+        ( -1, 'm', 'mili'  ),
+        ( -2, 'u', 'micro' ),
+        ( -3, 'n', 'nano'  ),
+    ]
+    try:
+        factor, tshort, tlong = next(filter(lambda x: num > (1000 ** x[0]), prefixes))
+    except StopIteration:
+        factor, tshort, tlong = prefixes[-1]
+    prefix = tlong if long_prefix else tshort
+    return num * (1000 ** -factor), prefix
