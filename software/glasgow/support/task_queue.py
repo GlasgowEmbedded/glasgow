GlasgowEmbedded/glasgow

commit a7d041f4ca5805a136acc8a99689833dedefd9d5

support.task_queue: improve cancellation support.
  * Propagate cancellation out of poll(), wait_one(), and wait_all().
  * Make cancel() an async function that waits until cancellation
    is finished.

Before this commit, cancelling a demultiplexer read (e.g. with ^C)
that was waiting on USB would cause it to fail an assert. After this
commit, the read is also cancelled.

software/glasgow/support/task_queue.py
@@ -21,8 +21,7 @@ class TaskQueue:
 
     def _callback(self, future):
         self._live.remove(future)
-        if not future.cancelled():
-            self._done.append(future)
+        self._done.append(future)
 
     def submit(self, coro):
         """
@@ -33,16 +32,18 @@ class TaskQueue:
         self._live.add(future)
         future.add_done_callback(self._callback)
 
-    def cancel(self):
+    async def cancel(self):
         """
-        Cancel all tasks in the queue.
+        Cancel all tasks in the queue, and wait until cancellation is finished.
         """
         for task in self._live:
             task.cancel()
+        if self._live:
+            await asyncio.wait(self._live, return_when=asyncio.ALL_COMPLETED)
 
     async def poll(self):
         """
-        Await all finished tasks that have been submitted to the queue. Return ``True`` if there
+        Await all finished tasks that have been submitted to the queue. Returns ``True`` if there
         were any finished tasks, ``False`` otherwise.
 
         This method needs to be called regularly to ensure that exceptions are propagated upwards
@@ -56,12 +57,11 @@ class TaskQueue:
     async def wait_one(self):
         """
         Await at least one task in the queue. If there are no finished tasks, waits until the first
-        pending task finishes. Equivalent to :meth:`poll` otherwise.
+        pending task finishes.
         """
         if not self._done:
             await asyncio.wait(self._live, return_when=asyncio.FIRST_COMPLETED)
-        assert len(self._done) > 0
-        return await self.poll()
+        await self.poll()
 
     async def wait_all(self):
         """
@@ -69,7 +69,7 @@ class TaskQueue:
         """
         if self._live:
             await asyncio.wait(self._live, return_when=asyncio.ALL_COMPLETED)
-        return await self.poll()
+        await self.poll()
 
     def __bool__(self):
         return bool(self._live)

software/glasgow/access/direct/demultiplexer.py
@@ -180,11 +180,8 @@ class DirectDemultiplexerInterface(AccessDemultiplexerInterface):
     async def cancel(self):
         if self._in_tasks or self._out_tasks:
             self.logger.trace("FIFO: cancelling operations")
-            self._in_tasks .cancel()
-            self._out_tasks.cancel()
-            # libusb cancellation is asynchronous, so wait until it's actually done.
-            await self._in_tasks .wait_all()
-            await self._out_tasks.wait_all()
+            await self._in_tasks .cancel()
+            await self._out_tasks.cancel()
 
     async def reset(self):
         await self.cancel()

commit 1dbfc8ea685127555f23d7cc2daffbad3c8b6e4e

support.task_queue: clear done queue after cancellation.
Since .cancel() waits until all live tasks are done, it is likely
that the done queue will contain some cancelled tasks. If the done
queue is not cleared, then the next poll()/wait_one()/wait_all() call
will cancel the caller, which usually silently terminates it. In any
case, semantically .cancel() discards the results of all tasks in
the queue even if they completed before they were cancelled.

This bug was introduced in commit a7d041f4.

Fixes #202.

Co-authored-by: Filipe Laíns <lains@archlinux.org>

software/glasgow/support/task_queue.py
@@ -40,6 +40,7 @@ class TaskQueue:
             task.cancel()
         if self._live:
             await asyncio.wait(self._live, return_when=asyncio.ALL_COMPLETED)
+        self._done.clear()
 
     async def poll(self):
         """