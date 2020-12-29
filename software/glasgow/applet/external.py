9ba025e8495235efac0e9542d3d600e96e7da093
Attie Grande <attie.grande@argentum-systems.co.uk>
2020-12-29 08:20:46 +0000

    applet: add exception handling around loading out-of-tree applets

--- software/glasgow/applet/external.py
+++ software/glasgow/applet/external.py
@@ -1,6 +1,17 @@
 import os
+import sys
+import logging
+import traceback
 import pkg_resources
+
+logger = logging.getLogger(__name__)
 
 if "GLASGOW_DISABLE_OOT" not in os.environ:
     for entry_point in pkg_resources.iter_entry_points('glasgow'):
-        entry_point.load()
+        try:
+            entry_point.load()
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            exc_frame = traceback.extract_tb(exc_tb)[-1]
+            exc_source = "{}:{}".format(exc_frame.filename, exc_frame.lineno)
+            logger.error("failed to load external applet: %s: %s: %s", entry_point.name, exc_source, e)
