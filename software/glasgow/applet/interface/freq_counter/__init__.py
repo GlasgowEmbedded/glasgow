8d8f40ef2f98de702c9c0ac301bffd1279825691
Attie Grande <attie.grande@argentum-systems.co.uk>
2020-12-30 18:33:12 +0000

    WIP: move control into FrequencyCounterInterface()

--- software/glasgow/applet/interface/freq_counter/__init__.py
+++ software/glasgow/applet/interface/freq_counter/__init__.py
@@ -47,6 +47,46 @@ class FrequencyCounterSubtarget(Elaboratable):
 
         return m
 
+class FrequencyCounterInterface:
+    def __init__(self, applet, device, interface):
+        self.applet = applet
+        self.device = device
+        self.lower = interface
+
+    async def configure(self, duration=2.0):
+        ctr = int(self.applet.sys_clk_freq * duration)
+
+        # this is broken (see comment below)
+        #await self.device.write_register(self.applet.__reg_clk_count, ctr, width=4)
+
+        await self.applet.set_clk_count(ctr)
+
+    async def start(self):
+        await self.lower.write([ _Command.GO ])
+        await self.lower.flush()
+
+    async def is_running(self):
+        return await self.applet.get_running()
+
+    async def wait(self):
+        while await self.is_running():
+            await asyncio.sleep(0.1)
+
+    async def get_result(self):
+        clk_count = await self.applet.get_clk_count()
+        edge_count = await self.applet.get_edge_count()
+
+        sample_duration = clk_count / self.applet.sys_clk_freq
+        signal_freq = edge_count / sample_duration
+
+        return signal_freq
+
+    async def measure(self, duration=2.0):
+        await self.configure(duration)
+        await self.start()
+        await self.wait()
+        return await self.get_result()
+
 class FrequencyCounterApplet(GlasgowApplet, name="freq-counter"):
     logger = logging.getLogger(__name__)
     help = "frequency counter"
@@ -85,24 +125,53 @@ class FrequencyCounterApplet(GlasgowApplet, name="freq-counter"):
     def add_run_arguments(cls, parser, access):
         super().add_run_arguments(parser, access)
 
-    async def measure(self, device, args, clk_count):
-        iface = await device.demultiplexer.claim_interface(self, self.mux_interface, args)
-
-        await device.write_register(self.__reg_clk_count, clk_count, width=4)
-
-        await iface.write([ _Command.GO ])
-        await iface.flush()
-
-        while await device.read_register(self.__reg_running, width=1):
-            await asyncio.sleep(0.1)
-
-        edge_count = await device.read_register(self.__reg_edge_count, width=4)
-
-        sample_duration = clk_count / self.sys_clk_freq
-        signal_freq = edge_count / sample_duration
-
-        return signal_freq
-
     async def run(self, device, args):
-        signal_freq = await self.measure(device, args, int(self.sys_clk_freq * args.duration))
+        self.device = device
+
+        iface = await device.demultiplexer.claim_interface(self, self.mux_interface, args)
+        freq_ctr = FrequencyCounterInterface(self, device, iface)
+
+        signal_freq = await freq_ctr.measure(args.duration)
         print('signal frequency: {:>7.3f} {:1}Hz'.format( *num_to_si(signal_freq) ))
+
+    # TODO: for some reason, accessing the registers from the FrequencyCounterInterface
+    #       class will raise an odd / malformed AttributeException... as below. This exception
+    #       isn't raised by GlasgowHardwareDevice.write_register(), but appears to occur on the
+    #       return - wrapping below with a try / except / pass effectively resolves the issue,
+    #       but A) that's disgusting, and B) it still breaks assignment / register_read() calls.
+    #
+    #       for the moment, I've put proxy functions here, but I'd like to remove them...?
+    #
+    #   $ glasgow run freq-counter -V 3.3
+    #   I: g.device.hardware: device already has bitstream ID 171709aadf51812cc9d1e3e54e881a43
+    #   I: g.cli: running handler for applet 'freq-counter'
+    #   I: g.applet.interface.freq_counter: port(s) A, B voltage set to 3.3 V
+    #   Traceback (most recent call last):
+    #     File "/home/attie/proj_local/glasgow/venv/bin/glasgow", line 11, in <module>
+    #       load_entry_point('glasgow', 'console_scripts', 'glasgow')()
+    #     File "/home/attie/proj_local/glasgow/glasgow/software/glasgow/cli.py", line 857, in main
+    #       exit(loop.run_until_complete(_main()))
+    #     File "/home/attie/.bin/python3.8.2/lib/python3.8/asyncio/base_events.py", line 616, in run_until_complete
+    #       return future.result()
+    #     File "/home/attie/proj_local/glasgow/glasgow/software/glasgow/cli.py", line 650, in _main
+    #       task.result()
+    #     File "/home/attie/proj_local/glasgow/glasgow/software/glasgow/cli.py", line 600, in run_applet
+    #       iface = await applet.run(device, args)
+    #     File "/home/attie/proj_local/glasgow/glasgow/software/glasgow/applet/interface/freq_counter/__init__.py", line 136, in run
+    #       signal_freq = await freq_ctr.measure(args.duration)
+    #     File "/home/attie/proj_local/glasgow/glasgow/software/glasgow/applet/interface/freq_counter/__init__.py", line 85, in measure
+    #       await self.configure(duration)
+    #     File "/home/attie/proj_local/glasgow/glasgow/software/glasgow/applet/interface/freq_counter/__init__.py", line 60, in configure
+    #       await self.device.write_register(self.applet.__reg_clk_count, ctr, width=4)
+    #   AttributeError: 'FrequencyCounterApplet' object has no attribute '_FrequencyCounterInterface__reg_clk_count'
+
+    async def get_clk_count(self):
+        return await self.device.read_register(self.__reg_clk_count, width=4)
+    async def set_clk_count(self, value):
+        await self.device.write_register(self.__reg_clk_count, value, width=4)
+
+    async def get_edge_count(self):
+        return await self.device.read_register(self.__reg_edge_count, width=4)
+
+    async def get_running(self):
+        return bool(await self.device.read_register(self.__reg_running, width=1))
