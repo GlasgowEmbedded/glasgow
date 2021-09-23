cbfc6404951c44a3105ae5685b0bf62253e3b2e7
Attie Grande <attie.grande@argentum-systems.co.uk>
2021-09-23 23:31:51 +0100

    applet.program.avr: add support for extended addressing

--- software/glasgow/applet/program/avr/spi/__init__.py
+++ software/glasgow/applet/program/avr/spi/__init__.py
@@ -16,6 +16,7 @@ class ProgramAVRSPIInterface(ProgramAVRInterface):
         self._logger = logger
         self._level  = logging.DEBUG if self._logger.name == __name__ else logging.TRACE
         self._addr_dut_reset = addr_dut_reset
+        self._extended_addr  = None
 
     def _log(self, message, *args):
         self._logger.log(self._level, "AVR SPI: " + message, *args)
@@ -104,7 +105,15 @@ class ProgramAVRSPIInterface(ProgramAVRInterface):
         _, _, _, data = await self._command(0b0011_1000, 0b0000_0000, address, 0)
         return data
 
+    async def load_extended_address_byte(self, address):
+        extended_addr = (address >> 17) & 0xff
+        if self._extended_addr != extended_addr:
+            self._log("load extended address %#02x", extended_addr)
+            await self._command(0b0100_1101, 0, extended_addr, 0)
+            self._extended_addr = extended_addr
+
     async def read_program_memory(self, address):
+        await self.load_extended_address_byte(address)
         self._log("read program memory address %#06x", address)
         _, _, _, data = await self._command(
             0b0010_0000 | (address & 1) << 3,
@@ -122,6 +131,7 @@ class ProgramAVRSPIInterface(ProgramAVRInterface):
             data)
 
     async def write_program_memory_page(self, address):
+        await self.load_extended_address_byte(address)
         self._log("write program memory page at %#06x", address)
         await self._command(
             0b0100_1100,
