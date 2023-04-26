79b583c9eccafb084a5503669d5bd87d3d7c6685
bobtwinkles <srkoser+github@gmail.com>
2023-04-25 17:43:55 -0700

    applet.video.rgb_input: convert to native amaranth
    Since FSMs are no longer objects that can have resets bound to them, the
    parts of the capture machinery that need to be reset by a watchdog are
    broken out in to a submodule

--- software/glasgow/applet/video/rgb_input/__init__.py
+++ software/glasgow/applet/video/rgb_input/__init__.py
@@ -1,123 +1,189 @@
 import logging
 import math
-from amaranth.compat import *
-from amaranth.compat.genlib.cdc import *
+from amaranth import *
+from amaranth.lib.cdc import FFSynchronizer
+from amaranth.hdl.rec import Record
 
 from ... import *
 
+_bus_layout = [
+    ("r", 5),
+    ("g", 5),
+    ("b", 5),
+    ("dck", 1),
+]
 
-class VideoRGBInputSubtarget(Module):
+class VideoRGBCaptureMachine(Elaboratable):
+    def __init__(self, bus, in_fifo, rows, columns):
+        """
+        Core of the capture machinery. Takes color data from ``bus`` and
+        delivers it to ``in_fifo``.
+
+        Expects the signals arriving on ``bus`` to be synchronized to the
+        ``sync`` domain already.
+        """
+        self.bus = bus
+        self.in_fifo = in_fifo
+        self.rows = rows
+        self.columns = columns
+
+    def elaborate(self, platform):
+        m = Module()
+
+        # Strobe based on the external data clock
+        dck_r = Signal()
+        stb   = Signal()
+        m.d.sync += [
+            dck_r.eq(self.bus.dck),
+            stb.eq(~dck_r & self.bus.dck)
+        ]
+
+        # bind to the input fifo, tracking overflow
+        we    = Signal.like(self.in_fifo.we)
+        din   = Signal.like(self.in_fifo.din)
+        ovf   = Signal()
+        ovf_c = Signal()
+        m.d.comb += [
+            self.in_fifo.we.eq(we & ~ovf),
+            self.in_fifo.din.eq(din),
+        ]
+        m.d.sync += \
+            ovf.eq(~ovf_c & (ovf | (we & ~self.in_fifo.writable)))
+
+        # frame counter and corresponding strobe
+        frame = Signal(5)
+        f_stb = Signal()
+        with m.If(f_stb):
+            m.d.sync += frame.eq(frame + 1)
+
+        # state machine registers
+        # previous frame overflow indicator and row/column counters
+        ovf_r = Signal()
+        row   = Signal(range(self.rows))
+        col   = Signal(range(self.columns))
+
+        # when our internal strobe goes high, latch a pixel
+        pixel = Signal(15)
+        with m.If(stb):
+            m.d.sync += pixel.eq(Cat(self.bus.r, self.bus.g, self.bus.b))
+
+        with m.FSM(reset="CAPTURE-ROW") as fsm:
+            # initial state: begin capture of a row, clearing the overflow flag
+            # and skipping the first pixel of the first row because we latch
+            # pixels 1 clock behind the data line.
+            with m.State("CAPTURE-ROW"):
+                with m.If(stb):
+                    with m.If(row == 0):
+                        m.d.comb += [
+                            ovf_r.eq(ovf),
+                            ovf_c.eq(1),
+                            f_stb.eq(1),
+                        ]
+                        m.next = "SKIP-FIRST-PIXEL"
+                    with m.Elif(row == self.rows):
+                        m.d.sync += row.eq(0)
+                        m.next = "REPORT-FRAME"
+                    with m.Else():
+                        m.next= "REPORT-FRAME"
+            # single-dclk delay for alignment
+            with m.State("SKIP-FIRST-PIXEL"):
+                with m.If(stb):
+                    m.next = "REPORT-FRAME"
+            # send frame marker including frame counter, row counter, and
+            # indicator that the previous frame overflowed
+            with m.State("REPORT-FRAME"):
+                m.d.comb += [
+                    din.eq(0x80 | (ovf_r << 7) | (frame << 1) | (row >> 7)),
+                    we.eq(1),
+                ]
+                m.next = "REPORT-ROW"
+            # send row start marker
+            with m.State("REPORT-ROW"):
+                m.d.comb += [
+                    din.eq(row & 0x7f),
+                    we.eq(1),
+                ]
+                m.next = ("REPORT-1")
+            # use the next 3 clocks to send the 3 color channels
+            for (state, offset, nextstate) in (
+                ("REPORT-1",  0, "REPORT-2"),
+                ("REPORT-2",  5, "REPORT-3"),
+                ("REPORT-3", 10, "CAPTURE-PIXEL")
+            ):
+                with m.State(state):
+                    m.d.comb += [
+                        din.eq((pixel >> offset) & 0x1f),
+                        we.eq(1)
+                    ]
+                    m.next = nextstate
+            # advance row/column counters
+            with m.State("CAPTURE-PIXEL"):
+                with m.If(stb):
+                    with m.If(col == self.columns - 1):
+                        m.d.sync += [
+                            col.eq(0),
+                            row.eq(row + 1),
+                        ]
+                        m.next = "CAPTURE-ROW"
+                    with m.Else():
+                        m.d.sync += col.eq(col + 1)
+                        m.next = "REPORT-1"
+
+
+        return m
+
+class VideoRGBInputSubtarget(Elaboratable):
     def __init__(self, rows, columns, vblank, pads, in_fifo, sys_clk_freq):
+        self.rows = rows
+        self.columns = columns
+        self.vblank = vblank
+        self.pads = pads
+        self.in_fifo = in_fifo
+        self.sys_clk_freq = sys_clk_freq
+
+
+    def elaborate(self, platform):
+        m = Module()
+
+        # Bring the parallel bus in to our internal clock domain and pack it in
+        # to a Record
         rx    = Signal(5)
         gx    = Signal(5)
         bx    = Signal(5)
         dck   = Signal()
-        self.specials += [
-            MultiReg(pads.r_t.i, rx),
-            MultiReg(pads.g_t.i, gx),
-            MultiReg(pads.b_t.i, bx),
-            MultiReg(pads.dck_t.i, dck)
+        m.submodules += [
+            FFSynchronizer(self.pads.r_t.i, rx),
+            FFSynchronizer(self.pads.g_t.i, gx),
+            FFSynchronizer(self.pads.b_t.i, bx),
+            FFSynchronizer(self.pads.dck_t.i, dck)
+        ]
+        pixel_bus = Record(_bus_layout)
+        m.d.comb += [
+            pixel_bus.r.eq(rx),
+            pixel_bus.g.eq(gx),
+            pixel_bus.b.eq(bx),
+            pixel_bus.dck.eq(dck),
         ]
 
-        dck_r = Signal()
-        stb   = Signal()
-        self.sync += [
-            dck_r.eq(dck),
-            stb.eq(~dck_r & dck)
-        ]
+        # watchdog that resets the capture machine when we don't see a
+        # transition on the dck line for >90% of a vblank
+        vblank_cyc = math.ceil(self.vblank * 0.9 * self.sys_clk_freq)
+        wdt        = Signal(range(vblank_cyc))
+        wdt_s      = Signal()
 
-        we    = Signal.like(in_fifo.we)
-        din   = Signal.like(in_fifo.din)
-        ovf   = Signal()
-        ovf_c = Signal()
-        self.comb += [
-            in_fifo.we.eq(we & ~ovf),
-            in_fifo.din.eq(din),
-        ]
-        self.sync += \
-            ovf.eq(~ovf_c & (ovf | (we & ~in_fifo.writable)))
+        with m.If(dck):
+            with m.If(wdt != vblank_cyc):
+                m.d.sync += wdt.eq(wdt + 1)
+        with m.Else():
+            m.d.sync += wdt.eq(0)
+        m.d.comb += wdt_s.eq(wdt == vblank_cyc)
 
-        pixel = Signal(15)
-        self.sync += \
-            If(stb, pixel.eq(Cat(rx, gx, bx)))
-
-        frame = Signal(5)
-        f_stb = Signal()
-        self.sync += \
-            If(f_stb, frame.eq(frame + 1))
-
-        ovf_r = Signal()
-        row   = Signal(max=rows)
-        col   = Signal(max=columns)
-        self.submodules.fsm = ResetInserter()(FSM(reset_state="CAPTURE-ROW"))
-        self.fsm.act("CAPTURE-ROW",
-            If(stb,
-                If(row == 0,
-                    ovf_r.eq(ovf),
-                    ovf_c.eq(1),
-                    f_stb.eq(1),
-                    NextState("SKIP-FIRST-PIXEL")
-                ).Elif(row == rows,
-                    NextValue(row, 0),
-                    NextState("REPORT-FRAME")
-                ).Else(
-                    NextState("REPORT-FRAME")
-                )
-            )
-        )
-        self.fsm.act("SKIP-FIRST-PIXEL",
-            If(stb,
-                NextState("REPORT-FRAME")
-            )
-        )
-        self.fsm.act("REPORT-FRAME",
-            din.eq(0x80 | (ovf_r << 7) | (frame << 1) | (row >> 7)),
-            we.eq(1),
-            NextState("REPORT-ROW")
-        )
-        self.fsm.act("REPORT-ROW",
-            din.eq(row & 0x7f),
-            we.eq(1),
-            NextState("REPORT-1")
-        )
-        for (state, offset, nextstate) in (
-            ("REPORT-1",  0, "REPORT-2"),
-            ("REPORT-2",  5, "REPORT-3"),
-            ("REPORT-3", 10, "CAPTURE-PIXEL")
-        ):
-            self.fsm.act(state,
-                din.eq((pixel >> offset) & 0x1f),
-                we.eq(1),
-                NextState(nextstate)
-            )
-        self.fsm.act("CAPTURE-PIXEL",
-            If(stb,
-                If(col == columns - 1,
-                    NextValue(col, 0),
-                    NextValue(row, row + 1),
-                    NextState("CAPTURE-ROW")
-                ).Else(
-                    NextValue(col, col + 1),
-                    NextState("REPORT-1")
-                )
-            )
+        # Create and bind the actual capture machine
+        m.submodules.capture_machine = ResetInserter(wdt_s)(
+            VideoRGBCaptureMachine(pixel_bus, self.in_fifo, self.rows, self.columns)
         )
 
-        vblank_cyc = math.ceil(vblank * 0.9 * sys_clk_freq) # reset at 90% vblank
-        timer      = Signal(max=vblank_cyc)
-        self.sync += [
-            If(dck,
-                If(timer != vblank_cyc,
-                    timer.eq(timer + 1)
-                )
-            ).Else(
-                timer.eq(0)
-            )
-        ]
-        self.comb += [
-            self.fsm.reset.eq(timer == vblank_cyc),
-            # sync.oe.eq(self.fsm.reset),
-        ]
+        return m
 
 
 class VideoRGBInputApplet(GlasgowApplet, name="video-rgb-input"):
