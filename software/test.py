97e2ac0589f26f417f3c383f6078f92f11c514fc
Wanda <wanda@phinode.net>
2023-10-20 14:34:34 +0200

    software: get rid of test.py wrapper

--- software/test.py
+++
@@ -1,8 +0,0 @@
-# Top-level interface to run the Glasgow tests.
-
-import unittest
-from setuptools.command.test import ScanningLoader
-
-
-if __name__ == "__main__":
-    unittest.main(module="glasgow", testLoader=ScanningLoader())
