71bf5c1066b6e24da65f089cb11a097b07d6642a
Wanda <wanda@phinode.net>
2023-12-12 12:08:26 +0100

    applet.program.xc9500xl: major refactoring.
    New functionality:
    
    - XC9500XV support
    - support for programming read and write protection bits
    - added `--erase` and `--verify` options to `program` operation
    - blank check is performed before programming
    - detects operations failing due to read and write protection

--- software/glasgow/arch/xilinx/xc9500xl.py
+++ software/glasgow/arch/xilinx/xc9500xl.py
@@ -11,8 +11,16 @@ __all__ = [
     "IR_EXTEST", "IR_SAMPLE", "IR_INTEST", "IR_FBLANK", "IR_ISPEN", "IR_ISPENC",
     "IR_FPGM", "IR_FPGMI", "IR_FERASE", "IR_FBULK", "IR_FVFY", "IR_FVFYI", "IR_ISPEX",
     "IR_CLAMP", "IR_HIGHZ", "IR_USERCODE", "IR_IDCODE", "IR_BYPASS",
+    "IR_STATUS",
     # DR
     "DR_ISDATA", "DR_ISADDRESS", "DR_ISCONFIGURATION",
+    "CTRL_START", "CTRL_OK", "CTRL_WPROT", "CTRL_WORKING",
+    "ADDR_OVERRIDE_MAGIC",
+    # bitstream geometry
+    "BS_ROWS", "BS_COLS", "bs_address",
+    "USERCODE_BITS", "READ_PROT_BIT", "WRITE_PROT_BIT", "DONE_BIT",
+    # wait times
+    "WAIT_ERASE", "WAIT_PROGRAM", "WAIT_BLANK_CHECK", "WAIT_ISPEX",
 ]
 
 
@@ -35,24 +43,62 @@ IR_USERCODE = bits("11111101") # USERCODE[32]
 IR_IDCODE   = bits("11111110") # IDCODE[32]
 IR_BYPASS   = bits("11111111") # BYPASS[1]
 
+IR_STATUS = bitstruct("IR_STATUS", 8, [
+    ("_fixed1",       1),
+    ("_fixed0",       1),
+    ("write_protect", 1),
+    ("read_protect",  1),
+    ("isp_enabled",   1),
+    ("done",          1),
+    ("_unused",       2),
+])
 
-def DR_ISDATA(width):
-    return bitstruct("DR_ISDATA", 2 + width, [
-        ("valid",    1),
-        ("strobe",   1),
-        ("data", width),
+
+def DR_ISDATA(fbs):
+    return bitstruct("DR_ISDATA", 2 + fbs * 8, [
+        ("control",    2),
+        ("data", fbs * 8),
     ])
 
 DR_ISADDRESS = bitstruct("DR_ISADDRESS", 18, [
-    ("valid",    1),
-    ("strobe",   1),
+    ("control",  2),
     ("address", 16),
 ])
 
-def DR_ISCONFIGURATION(width):
-    return bitstruct("DR_ISCONFIGURATION", 18 + width, [
-        ("valid",    1),
-        ("strobe",   1),
-        ("data", width),
-        ("address", 16),
+def DR_ISCONFIGURATION(fbs):
+    return bitstruct("DR_ISCONFIGURATION", 18 + fbs * 8, [
+        ("control",    2),
+        ("data", fbs * 8),
+        ("address",   16),
     ])
+
+
+CTRL_WPROT   = 0
+CTRL_OK      = 1
+CTRL_WORKING = 2
+CTRL_START   = 3
+
+ADDR_OVERRIDE_MAGIC = 0xaa55
+
+
+BS_ROWS = 108
+BS_COLS = 15
+
+def bs_address(row, col):
+    return row << 5 | (col // 5) << 3 | (col % 5)
+
+USERCODE_BITS = [
+    (0, 7, 7 - i // 2, 6 + i % 2) for i in range(16)
+] + [
+    (0, 6, 7 - i // 2, 6 + i % 2) for i in range(16)
+]
+
+WRITE_PROT_BIT = (11, 0, 6)
+READ_PROT_BIT = (11, 3, 6)
+DONE_BIT = (0, 11, 6, 6)
+
+# Everything in µs.
+WAIT_ERASE = 200_000
+WAIT_PROGRAM = 20_000
+WAIT_BLANK_CHECK = 500
+WAIT_ISPEX = 100
--- software/glasgow/database/xilinx/xc9500xl.py
+++ software/glasgow/database/xilinx/xc9500xl.py
@@ -5,31 +5,19 @@ __all__ = ["devices", "devices_by_idcode", "devices_by_name"]
 
 
 XC9500XLDevice = namedtuple("XC9500XLDevice", (
-    "name", "idcode", "bitstream_words", "word_width", "usercode_low", "usercode_high"
+    "name", "idcode", "fbs", "kind",
 ))
 
 
 devices = [
-    XC9500XLDevice("XC95288XL",
-        idcode=(0x049, 0x9616),
-        bitstream_words=1620,
-        word_width=128,
-        usercode_low=90, usercode_high=105),
-    XC9500XLDevice("XC95144XL",
-        idcode=(0x049, 0x9608),
-        bitstream_words=1620,
-        word_width=64,
-        usercode_low=90, usercode_high=105),
-    XC9500XLDevice("XC9572XL",
-        idcode=(0x049, 0x9604),
-        bitstream_words=1620,
-        word_width=32,
-        usercode_low=90, usercode_high=105),
-    XC9500XLDevice("XC9536XL",
-        idcode=(0x049, 0x9602),
-        bitstream_words=1620,
-        word_width=16,
-        usercode_low=90, usercode_high=105),
+    XC9500XLDevice("XC9536XL", idcode=(0x049, 0x9602), fbs=2, kind="xl"),
+    XC9500XLDevice("XC9572XL", idcode=(0x049, 0x9604), fbs=4, kind="xl"),
+    XC9500XLDevice("XC95144XL", idcode=(0x049, 0x9608), fbs=8, kind="xl"),
+    XC9500XLDevice("XC95288XL", idcode=(0x049, 0x9616), fbs=16, kind="xl"),
+    XC9500XLDevice("XC9536XV", idcode=(0x049, 0x9702), fbs=2, kind="xv"),
+    XC9500XLDevice("XC9572XV", idcode=(0x049, 0x9704), fbs=4, kind="xv"),
+    XC9500XLDevice("XC95144XV", idcode=(0x049, 0x9708), fbs=8, kind="xv"),
+    XC9500XLDevice("XC95288XV", idcode=(0x049, 0x9716), fbs=16, kind="xv"),
 ]
 
 devices_by_idcode = defaultdict(lambda: None,
