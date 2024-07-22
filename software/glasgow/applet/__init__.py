9cdad0691235952404d255b53a7132c8dc271594
Catherine <whitequark@whitequark.org>
2024-07-22 07:08:27 +0000

    applet: support async context managers in record/replay tests.

--- software/glasgow/applet/__init__.py
+++ software/glasgow/applet/__init__.py
@@ -1,6 +1,7 @@
 import re
 import argparse
 from abc import ABCMeta, abstractmethod
+from contextlib import AbstractAsyncContextManager, asynccontextmanager
 from amaranth import *
 
 from ..support.arepl import *
@@ -145,10 +146,10 @@ class MockRecorder:
         json.dump(fp=self.__fixture, default=self.__dump_object, obj=stanza)
         self.__fixture.write("\n")
 
-    def __dump_method(self, method, args, kwargs, result, coro):
+    def __dump_method(self, call, kind, args, kwargs, result):
         self.__dump_stanza({
-            "method": method,
-            "async":  coro,
+            "call":   call,
+            "kind":   kind,
             "args":   args,
             "kwargs": kwargs,
             "result": result
@@ -159,14 +160,26 @@ class MockRecorder:
         if inspect.ismethod(mocked):
             def wrapper(*args, **kwargs):
                 result = mocked(*args, **kwargs)
-                if inspect.isawaitable(result):
+                if isinstance(result, AbstractAsyncContextManager):
+                    @asynccontextmanager
+                    async def cmgr_wrapper():
+                        value = await result.__aenter__()
+                        self.__dump_method(attr, "asynccontext.enter", (), {}, value)
+                        try:
+                            yield value
+                        finally:
+                            exc_type, exc_value, traceback = sys.exc_info()
+                            self.__dump_method(attr, "asynccontext.exit", (exc_value,), {}, None)
+                            await result.__aexit__(exc_type, exc_value, traceback)
+                    return cmgr_wrapper()
+                elif inspect.isawaitable(result):
                     async def coro_wrapper():
                         coro_result = await result
-                        self.__dump_method(attr, args, kwargs, coro_result, coro=True)
+                        self.__dump_method(attr, "asyncmethod", args, kwargs, coro_result)
                         return coro_result
                     return coro_wrapper()
                 else:
-                    self.__dump_method(attr, args, kwargs, result, coro=False)
+                    self.__dump_method(attr, "method", args, kwargs, result)
                     return result
             return wrapper
 
@@ -194,19 +207,47 @@ class MockReplayer:
         json_str = self.__fixture.readline()
         return json.loads(s=json_str, object_hook=self.__load_object)
 
+    @staticmethod
+    def __upgrade(stanza):
+        """Upgrade an object to the latest schema."""
+        if "method" in stanza:
+            stanza["call"] = stanza.pop("method")
+            if stanza.pop("async"):
+                stanza["kind"] = "asyncmethod"
+            else:
+                stanza["kind"] = "method"
+        return stanza
+
     def __getattr__(self, attr):
-        json_obj = self.__load()
-        self.__case.assertEqual(attr, json_obj["method"])
-        if json_obj["async"]:
+        stanza = self.__upgrade(self.__load())
+        self.__case.assertEqual(attr, stanza["call"])
+        if stanza["kind"] == "asynccontext.enter":
+            @asynccontextmanager
+            async def mock():
+                assert () == tuple(stanza["args"])
+                assert {} == stanza["kwargs"]
+                try:
+                    yield stanza["result"]
+                finally:
+                    exc_type, exc_value, traceback = sys.exc_info()
+                    exit_stanza = self.__load()
+                    self.__case.assertEqual(attr, exit_stanza["call"])
+                    self.__case.assertEqual("asynccontext.exit", exit_stanza["kind"])
+                    self.__case.assertEqual((exc_value,), tuple(exit_stanza["args"]))
+                    assert {} == exit_stanza["kwargs"]
+                    assert None == exit_stanza["result"]
+        elif stanza["kind"] == "asyncmethod":
             async def mock(*args, **kwargs):
-                self.__case.assertEqual(args, tuple(json_obj["args"]))
-                self.__case.assertEqual(kwargs, json_obj["kwargs"])
-                return json_obj["result"]
-        else:
+                self.__case.assertEqual(args, tuple(stanza["args"]))
+                self.__case.assertEqual(kwargs, stanza["kwargs"])
+                return stanza["result"]
+        elif stanza["kind"] == "method":
             def mock(*args, **kwargs):
-                self.__case.assertEqual(args, tuple(json_obj["args"]))
-                self.__case.assertEqual(kwargs, json_obj["kwargs"])
-                return json_obj["result"]
+                self.__case.assertEqual(args, tuple(stanza["args"]))
+                self.__case.assertEqual(kwargs, stanza["kwargs"])
+                return stanza["result"]
+        else:
+            assert False, f"unknown stanza {stanza['kind']}"
         return mock
 
 
