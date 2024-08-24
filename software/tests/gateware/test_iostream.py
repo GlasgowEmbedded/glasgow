e1d96db549ac39fdcc432397dfb617f87a6991f1
Purdea Andrei <andrei@purdea.ro>
2024-08-24 21:42:49 +0300

    test_iostream: refactor input sample tests so they can be reused

--- software/tests/gateware/test_iostream.py
+++ software/tests/gateware/test_iostream.py
@@ -12,18 +12,33 @@ async def stream_get(ctx, stream):
     ctx.set(stream.ready, 0)
     return payload
 
+async def stream_get_maybe(ctx, stream):
+    ctx.set(stream.ready, 1)
+    _, _, payload, stream_valid = await ctx.tick().sample(stream.payload, stream.valid)
+    ctx.set(stream.ready, 0)
+    if stream_valid:
+        return payload
+    else:
+        return None
+
 async def stream_put(ctx, stream, payload):
     ctx.set(stream.payload, payload)
     ctx.set(stream.valid, 1)
     await ctx.tick().until(stream.ready)
     ctx.set(stream.valid, 0)
 
+class IOStreamTimeoutError(Exception):
+    pass
+
 class IOStreamTestCase(unittest.TestCase):
-    def test_sdr_input_sampled_correctly(self):
+    def _subtest_sdr_input_sampled_correctly(self, o_valid_bits, i_en_bits, i_ready_bits, timeout_clocks=None):
         """ This is a latency-agnostic test, that verifies that the IOStreamer samples the inputs at the same time as the output signals change """
         ports = PortGroup()
         ports.clk_out = io.SimulationPort("o", 1)
         ports.data_in = io.SimulationPort("i", 8)
+
+        if timeout_clocks is None:
+            timeout_clocks = len(o_valid_bits) + len(i_ready_bits) + 20
 
         dut = IOStreamer({
             "clk_out": ("o", 1),
@@ -32,6 +47,7 @@ class IOStreamTestCase(unittest.TestCase):
 
         expected_sample = []
         actually_sampled = []
+        i_stream_consumer_finished = False
 
         async def input_generator_tb(ctx):
             """
@@ -62,9 +78,20 @@ class IOStreamTestCase(unittest.TestCase):
             """
             This testbench saves all the samples coming in over i_stream
             """
-            while True:
-                payload = await stream_get(ctx,dut.i_stream)
-                actually_sampled.append(payload.port.data_in.i)
+            nonlocal i_stream_consumer_finished
+            for i_ready_bit in i_ready_bits:
+                if i_ready_bit == "1":
+                    payload = await stream_get_maybe(ctx,dut.i_stream)
+                    if payload is not None:
+                        actually_sampled.append(payload.port.data_in.i)
+                else:
+                    await ctx.tick()
+            i_stream_consumer_finished = True
+
+
+        async def check_timeout_tb(ctx):
+            await ctx.tick().repeat(timeout_clocks)
+            raise IOStreamTimeoutError("Testcase timeout")
 
         async def main_testbench(ctx):
             """
@@ -75,32 +102,61 @@ class IOStreamTestCase(unittest.TestCase):
             """
             await ctx.tick()
 
-            for i in range(0,8):
-                await stream_put(ctx, dut.o_stream, {"meta": i, "i_en": i % 2, "port": { "clk_out": {"o": i % 2}}})
+            expected_samples_count = 0
 
-            await stream_put(ctx, dut.o_stream, {"meta": 0, "i_en": 0, "port": { "clk_out": {"o": 0}}})
+            for i in range(len(o_valid_bits)):
+                o_valid_bit = 1 if o_valid_bits[i] == "1" else 0
+                i_en_bit = 1 if i_en_bits[i] == "1" else 0
+                if o_valid_bit:
+                    if i_en_bit:
+                        expected_samples_count += 1
+                    await stream_put(ctx, dut.o_stream, {
+                        "meta": i,
+                        "i_en": i_en_bit,
+                        "port": {
+                            "clk_out": {
+                                "o": i_en_bit,
+                            }
+                        }
+                    })
+                else:
+                    await ctx.tick()
 
-            for i in range(20):
+            while not i_stream_consumer_finished:
                 await ctx.tick()
-            assert len(actually_sampled) == 4 # This should be checked as well, because a possible failure mode is
-            # if IOStreamer never generates clock edges. We don't want to end up comparing two empty lists against
-            # eachother.
-            assert actually_sampled == expected_sample
+
+            assert len(actually_sampled) == expected_samples_count # This should be checked as well, because a
+            # possible failure mode is if IOStreamer never generates clock edges. We don't want to end up
+            # comparing two empty lists against eachother.
+            assert actually_sampled == expected_sample, (f"Expected [" +
+                    ", ".join(f"0x{s:02x}" for s in expected_sample) +
+                    "] Got [" +
+                    ", ".join(f"0x{s:02x}" for s in actually_sampled) + "]")
 
         sim = Simulator(dut)
         sim.add_clock(1e-6)
         sim.add_testbench(main_testbench)
-        sim.add_testbench(i_stream_consumer_tb, background = True)
+        sim.add_testbench(i_stream_consumer_tb, background=True)
         sim.add_testbench(input_generator_tb, background=True)
         sim.add_testbench(save_expected_sample_values_tb, background=True)
+        sim.add_testbench(check_timeout_tb, background=True)
         with sim.write_vcd("test.vcd", fs_per_delta=1):
             sim.run()
 
-    def test_ddr_input_sampled_correctly(self):
+    def test_sdr_input_sampled_correctly(self):
+        self._subtest_sdr_input_sampled_correctly(
+            o_valid_bits = "111111111",
+            i_en_bits    = "010101010",
+            i_ready_bits = "111111111" + ("1"*20))
+
+    def _subtest_ddr_input_sampled_correctly(self, o_valid_bits, i_en_bits, i_ready_bits, timeout_clocks=None):
         """ This is a latency-agnostic test, that verifies that the IOStreamer samples the inputs at the same time as the output signals change """
         ports = PortGroup()
         ports.clk_out = io.SimulationPort("o", 1)
         ports.data_in = io.SimulationPort("i", 8)
+
+        if timeout_clocks is None:
+            timeout_clocks = len(o_valid_bits) + len(i_ready_bits) + 20
 
         CLK_PERIOD = 1e-6
 
@@ -111,6 +167,7 @@ class IOStreamTestCase(unittest.TestCase):
 
         expected_sample = []
         actually_sampled = []
+        i_stream_consumer_finished = False
 
         async def input_generator_tb(ctx):
             """
@@ -161,10 +218,20 @@ class IOStreamTestCase(unittest.TestCase):
             """
             This testbench saves all the samples coming in over i_stream
             """
-            while True:
-                payload = await stream_get(ctx,dut.i_stream)
-                data = payload.port.data_in.i[0], payload.port.data_in.i[1]
-                actually_sampled.append(data)
+            nonlocal i_stream_consumer_finished
+            for i_ready_bit in i_ready_bits:
+                if i_ready_bit == "1":
+                    payload = await stream_get_maybe(ctx,dut.i_stream)
+                    if payload is not None:
+                        data = payload.port.data_in.i[0], payload.port.data_in.i[1]
+                        actually_sampled.append(data)
+                else:
+                    await ctx.tick()
+            i_stream_consumer_finished = True
+
+        async def check_timeout_tb(ctx):
+            await ctx.tick().repeat(timeout_clocks)
+            raise IOStreamTimeoutError("Testcase timeout")
 
         async def main_testbench(ctx):
             """
@@ -175,16 +242,32 @@ class IOStreamTestCase(unittest.TestCase):
             """
             await ctx.tick()
 
-            for i in range(0,8):
-                await stream_put(ctx, dut.o_stream, {"meta": i, "i_en": i % 2, "port": { "clk_out": {"o": (i % 2, 0)}}})
+            expected_samples_count = 0
 
-            await stream_put(ctx, dut.o_stream, {"meta": 0, "i_en": 0, "port": { "clk_out": {"o": (0, 0)}}})
+            for i in range(len(o_valid_bits)):
+                o_valid_bit = 1 if o_valid_bits[i] == "1" else 0
+                i_en_bit = 1 if i_en_bits[i] == "1" else 0
+                if o_valid_bit:
+                    if i_en_bit:
+                        expected_samples_count += 1
+                    await stream_put(ctx, dut.o_stream, {
+                        "meta": i,
+                        "i_en": i_en_bit,
+                        "port": {
+                            "clk_out": {
+                                "o": (i_en_bit, 0),
+                            }
+                        }
+                    })
+                else:
+                    await ctx.tick()
 
-            for i in range(20):
+            while not i_stream_consumer_finished:
                 await ctx.tick()
-            assert len(actually_sampled) == 4 # This should be checked as well, because a possible failure mode is
-            # if IOStreamer never generates clock edges. We don't want to end up comparing two empty lists against
-            # eachother.
+
+            assert len(actually_sampled) == expected_samples_count # This should be checked as well, because a
+            # possible failure mode is if IOStreamer never generates clock edges. We don't want to end up
+            # comparing two empty lists against eachother.
 
             assert actually_sampled == expected_sample, (f"Expected [" +
                     ", ".join(f"(0x{s0:02x}, 0x{s1:02x})" for s0, s1 in expected_sample) +
@@ -194,11 +277,18 @@ class IOStreamTestCase(unittest.TestCase):
         sim = Simulator(dut)
         sim.add_clock(CLK_PERIOD)
         sim.add_testbench(main_testbench)
-        sim.add_testbench(i_stream_consumer_tb, background = True)
+        sim.add_testbench(i_stream_consumer_tb, background=True)
         sim.add_testbench(input_generator_tb, background=True)
         sim.add_testbench(save_expected_sample_values_tb, background=True)
+        sim.add_testbench(check_timeout_tb, background=True)
         with sim.write_vcd("test.vcd", fs_per_delta=1):
             sim.run()
+
+    def test_ddr_input_sampled_correctly(self):
+        self._subtest_ddr_input_sampled_correctly(
+            o_valid_bits = "111111111",
+            i_en_bits    = "010101010",
+            i_ready_bits = "111111111" + ("1"*20))
 
     def test_basic(self):
         ports = PortGroup()
