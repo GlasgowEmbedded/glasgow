66bf069325bdb17cde3042ba6d28b206099a3844
Aaron Jackson <aaron@aaronsplace.co.uk>
2025-05-07 23:21:36 +0100

    oops

--- software/glasgow/applet/interface/gpib_controller/test.py
+++ software/glasgow/applet/interface/gpib_controller/test.py
@@ -3,7 +3,7 @@ from amaranth import *
 from ... import *
 from . import GPIBControllerApplet
 
-class GPIBControllerAppletTestCase(GlasgowAppletTestCase, applet=GPIBCommandApplet):
+class GPIBControllerAppletTestCase(GlasgowAppletTestCase, applet=GPIBControllerApplet):
     @synthesis_test
     def test_build(self):
         self.assertBuilds()
