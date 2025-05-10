GlasgowEmbedded/glasgow

commit c97d2d8e0e69e4e2051b92309c622f2af8e2c1eb

software: migrate to Amaranth 0.5.

software/glasgow/target/hardware.py
@@ -1,13 +1,13 @@
-import hashlib
 import os
 import sys
 import tempfile
 import shutil
 import logging
 import hashlib
-import platformdirs
 import pathlib
+import platformdirs
 from amaranth import *
+from amaranth.lib import io
 from amaranth.build import ResourceError
 
 from ..gateware.i2c import I2CTarget
@@ -39,22 +39,23 @@ class GlasgowHardwareTarget(Elaboratable):
             raise ValueError("Unknown revision")
 
         try:
-            self.platform.request("unused")
+            self.platform.request("unused", dir="-")
         except ResourceError:
             pass
 
         self._submodules = []
 
-        self.i2c_target = I2CTarget(self.platform.request("i2c"))
+        self.i2c_target = I2CTarget(self.platform.request("i2c", dir={"scl": "-", "sda": "-"}))
         self.registers = I2CRegisters(self.i2c_target)
 
-        self.fx2_crossbar = FX2Crossbar(self.platform.request("fx2", xdr={
-            "sloe": 1, "slrd": 1, "slwr": 1, "pktend": 1, "fifoadr": 1, "flag": 2, "fd": 2
+        self.fx2_crossbar = FX2Crossbar(self.platform.request("fx2", dir={
+            "sloe": "-", "slrd": "-", "slwr": "-", "pktend": "-", "fifoadr": "-",
+            "flag": "-", "fd": "-"
         }))
 
         self.ports = {
-            "A": (8, lambda n: self.platform.request("port_a", n)),
-            "B": (8, lambda n: self.platform.request("port_b", n)),
+            "A": (8, lambda n: self.platform.request("port_a", n, dir={"io": "-", "oe": "-"})),
+            "B": (8, lambda n: self.platform.request("port_b", n, dir={"io": "-", "oe": "-"})),
         }
 
         if multiplexer_cls:
@@ -93,7 +94,7 @@ class GlasgowHardwareTarget(Elaboratable):
                     pass
         for unused_pin in unused_pins:
             if hasattr(unused_pin, "oe"):
-                m.d.comb += unused_pin.oe.o.eq(0)
+                m.submodules += io.Buffer("o", unused_pin.oe)
 
         return m
 

software/glasgow/access/__init__.py
@@ -1,6 +1,6 @@
 from abc import ABCMeta, abstractmethod
 from amaranth import *
-from amaranth.lib.io import Pin
+from amaranth.lib import io
 import argparse
 
 from ..gateware.pads import Pads
@@ -69,7 +69,7 @@ class AccessMultiplexerInterface(Elaboratable, metaclass=ABCMeta):
         pass
 
     def get_pins(self, pins, name=None):
-        triple = Pin(width=len(pins), dir='io')
+        triple = io.Buffer.Signature("io", len(pins)).create()
         for n, pin in enumerate(pins):
             self.build_pin_tristate(pin, triple.oe, triple.o[n], triple.i[n])
 

software/glasgow/access/direct/multiplexer.py
@@ -1,5 +1,6 @@
 import logging
 from amaranth import *
+from amaranth.lib import io
 
 from .. import AccessMultiplexer, AccessMultiplexerInterface
 
@@ -24,8 +25,8 @@ class _FIFOReadPort(Elaboratable):
         self.width = fifo.width
         self.depth = fifo.depth
 
-        self._ctrl_en = Signal(reset=1)
-        self._data_en = Signal(reset=1)
+        self._ctrl_en = Signal(init=1)
+        self._data_en = Signal(init=1)
 
         self.r_en   = Signal()
         self.r_rdy  = Signal()
@@ -62,8 +63,8 @@ class _FIFOWritePort(Elaboratable):
         self.width = fifo.width
         self.depth = fifo.depth
 
-        self._ctrl_en = Signal(reset=1)
-        self._data_en = Signal(reset=1)
+        self._ctrl_en = Signal(init=1)
+        self._data_en = Signal(init=1)
 
         self.w_en   = Signal()
         self.w_rdy  = Signal()
@@ -153,8 +154,7 @@ class DirectMultiplexer(AccessMultiplexer):
 
 
 class DirectMultiplexerInterface(AccessMultiplexerInterface):
-    def __init__(self, applet, analyzer, registers, fx2_crossbar, pipe_num, pins,
-                 throttle):
+    def __init__(self, applet, analyzer, registers, fx2_crossbar, pipe_num, pins, throttle):
         assert throttle in ("full", "fifo", "none")
 
         super().__init__(applet, analyzer)
@@ -167,7 +167,7 @@ class DirectMultiplexerInterface(AccessMultiplexerInterface):
         self._fifos         = []
         self._pin_tristates = []
 
-        self.reset, self._addr_reset = self._registers.add_rw(1, reset=1)
+        self.reset, self._addr_reset = self._registers.add_rw(1, init=1)
         self.logger.debug("adding reset register at address %#04x", self._addr_reset)
 
     def elaborate(self, platform):
@@ -186,13 +186,15 @@ class DirectMultiplexerInterface(AccessMultiplexerInterface):
             m.submodules += fifo
 
         for pin_parts, oe, o, i in self._pin_tristates:
+            m.submodules += (io_buffer := io.Buffer("io", pin_parts.io))
             m.d.comb += [
-                pin_parts.io.oe.eq(oe),
-                pin_parts.io.o.eq(o),
-                i.eq(pin_parts.io.i),
+                io_buffer.oe.eq(oe),
+                io_buffer.o.eq(o),
+                i.eq(io_buffer.i),
             ]
             if hasattr(pin_parts, "oe"):
-                m.d.comb += pin_parts.oe.o.eq(oe)
+                m.submodules += (oe_buffer := io.Buffer("o", pin_parts.oe))
+                m.d.comb += oe_buffer.o.eq(oe)
 
         return m
 

software/glasgow/access/simulation/multiplexer.py
@@ -99,7 +99,7 @@ class SimulationMultiplexerInterface(AccessMultiplexerInterface):
 
         self.in_fifo = self._make_fifo(
             crossbar_side="read", logic_side="write", cd_logic=clock_domain, depth=depth)
-        self.in_fifo.flush = Signal(reset=auto_flush)
+        self.in_fifo.flush = Signal(init=auto_flush)
         return self.in_fifo
 
     def get_out_fifo(self, depth=512, clock_domain=None):

software/glasgow/applet/__init__.py
@@ -116,6 +116,7 @@ from ..target.hardware import *
 from ..device.simulation import *
 from ..device.hardware import *
 from ..target.toolchain import find_toolchain
+from ..platform.all import GlasgowPlatformRevAB
 
 
 __all__ += ["GlasgowAppletTestCase", "synthesis_test", "applet_simulation_test",
@@ -292,7 +293,7 @@ class GlasgowAppletTestCase(unittest.TestCase):
             await self.device.download_target(self.target.build_plan())
         else:
             # avoid UnusedElaboratable warning
-            Fragment.get(self.target, None)
+            Fragment.get(self.target, GlasgowPlatformRevAB())
 
         return await self.applet.run(self.device, self._parsed_args)
 

software/glasgow/applet/audio/yamaha_opx/__init__.py
@@ -165,7 +165,7 @@ class YamahaCPUBus(Elaboratable):
 
         self.a  = Signal(2)
 
-        self.oe = Signal(reset=1)
+        self.oe = Signal(init=1)
         self.di = Signal(8)
         self.do = Signal(8)
 

software/glasgow/applet/control/servo/__init__.py
@@ -34,8 +34,8 @@ class ServoChannel(wiring.Component):
         # the first millisecond is always high (whenever the channel is enabled), the second is
         # an encoding of the position and the rest is always low.
         period_timer = Signal(range(self.period_us * resolution),
-                              reset=self.period_us * resolution - 1)
-        with m.If(period_timer == period_timer.reset):
+                              init=self.period_us * resolution - 1)
+        with m.If(period_timer == period_timer.init):
             m.d.sync += period_timer.eq(0)
             m.d.sync += en_r.eq(self.en)
             m.d.sync += pos_r.eq(self.pos)
@@ -48,7 +48,7 @@ class ServoChannel(wiring.Component):
         pulse_timer = Signal(range(resolution))
         pulse_count = Signal.like(self.pos)
 
-        with m.If(period_timer == period_timer.reset):
+        with m.If(period_timer == period_timer.init):
             m.d.sync += self.out.eq(en_r)
             m.d.sync += pulse_en.eq(1)
 

software/glasgow/applet/display/pdi/__init__.py
@@ -401,7 +401,7 @@ class DisplayPDIApplet(GlasgowApplet):
 
         cog_power, self.__addr_cog_power = target.registers.add_rw(1)
         cog_disch, self.__addr_cog_disch = target.registers.add_rw(1)
-        cog_reset, self.__addr_cog_reset = target.registers.add_rw(1, reset=1)
+        cog_reset, self.__addr_cog_reset = target.registers.add_rw(1, init=1)
 
         if hasattr(iface.pads, "pwm_t"):
             cog_pwmen, self.__addr_cog_pwmen = target.registers.add_rw(1)

commit 2aa19046b1d8c54415c290573de536de69e52617

target.hardware: disable pullups on revC pins used for routing.
This fixes a regression introduced in commit c97d2d8e.

The pins in the `unused` resource are connected to another pin in order
to simplify routing. iCE40 has a weak pullup on all unused pins that
we have to disable or it will interfere with operation.

software/glasgow/platform/rev_c.py
@@ -112,7 +112,7 @@ class _GlasgowRevCPlatform(GlasgowICE40Platform):
 
         # On revC, these balls are shared with B6 and B7, respectively.
         # Since the default pin state is a weak pullup, we need to tristate them explicitly.
-        Resource("unused", 0, Pins("A6 B5", dir="oe"), Attrs(IO_STANDARD="SB_LVCMOS")),
+        Resource("unused", 0, Pins("A6 B5", dir="io"), Attrs(IO_STANDARD="SB_LVCMOS")),
     ]
     connectors  = [
         #                     1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21 22

software/glasgow/target/hardware.py
@@ -41,11 +41,6 @@ class GlasgowHardwareTarget(Elaboratable):
         else:
             raise ValueError("Unknown revision")
 
-        try:
-            self.platform.request("unused", dir="-")
-        except ResourceError:
-            pass
-
         self._submodules = []
 
         self.i2c_target = I2CTarget(self.platform.request("i2c", dir={"scl": "-", "sda": "-"}))
@@ -85,15 +80,22 @@ class GlasgowHardwareTarget(Elaboratable):
         m.d.comb += self.i2c_target.address.eq(0b0001000)
 
         unused_pins = []
-        for width, req in self.ports.values():
-            for n in range(width):
+        for width, request in self.ports.values():
+            for idx in range(width):
                 try:
-                    unused_pins.append(req(n))
+                    unused_pins.append(request(idx))
                 except ResourceError:
                     pass
-        for unused_pin in unused_pins:
+        for idx, unused_pin in enumerate(unused_pins):
             if hasattr(unused_pin, "oe"):
-                m.submodules += io.Buffer("o", unused_pin.oe)
+                m.submodules[f"unused_pin_{idx}"] = io.Buffer("o", unused_pin.oe)
+
+        try:
+            # See note in `rev_c.py`.
+            unused_balls = self.platform.request("unused", dir="-")
+            m.submodules[f"unused_balls"] = io.Buffer("io", unused_balls)
+        except ResourceError:
+            pass
 
         return m
 