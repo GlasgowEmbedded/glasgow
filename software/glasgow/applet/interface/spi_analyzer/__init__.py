16d6112d521db535519fbde6b28250f712dab5c7
Catherine <whitequark@whitequark.org>
2025-05-17 16:57:34 +0000

    applet.interface.spi_analyzer: significantly improve maximum SCK.
    The applet has been tested with a maximum SCK of 65 MHz.

--- software/glasgow/applet/interface/spi_analyzer/__init__.py
+++ software/glasgow/applet/interface/spi_analyzer/__init__.py
@@ -8,6 +8,7 @@ from amaranth.lib.wiring import In, Out
 from cobs.cobs import decode as cobs_decode
 
 from glasgow.support.logging import dump_hex
+from glasgow.gateware.stream import StreamFIFO
 from glasgow.gateware.cobs import Encoder as COBSEncoder
 from glasgow.applet import GlasgowAppletError, GlasgowAppletV2
 
@@ -19,172 +20,207 @@ class SPIAnalyzerOverflow(GlasgowAppletError):
     pass
 
 
-class SPIChannelAnalyzer(wiring.Component):
-    cs:   Out(io.Buffer.Signature("i", 1))
-    clk:  Out(io.Buffer.Signature("i", 1))
-    data: Out(io.Buffer.Signature("i", 1))
+class SPIAnalyzerFrontend(wiring.Component):
+    def __init__(self, ports, *, word_width=8):
+        self._ports = ports
 
-    stream: Out(stream.Signature(8))
-    flush:  Out(1)
+        self._word_width = word_width
 
-    overflow: Out(1)
-
-    def __init__(self, fifo_depth):
-        self._fifo_depth = fifo_depth
-
-        super().__init__()
+        super().__init__({
+            "stream": Out(stream.Signature(data.StructLayout({
+                "copi":  self._word_width,
+                "cipo":  self._word_width,
+                "epoch": 1
+            }))),
+            "complete": Out(1), # FIFO empty and CS# deasserted
+            "overflow": Out(1),
+        })
 
     def elaborate(self, platform):
         m = Module()
 
-        cs_sync   = Signal()
-        clk_sync  = Signal()
-        data_sync = Signal()
-        m.submodules.cs_sync   = cdc.FFSynchronizer(self.cs.i, cs_sync)
-        m.submodules.clk_sync  = cdc.FFSynchronizer(self.clk.i, clk_sync)
-        m.submodules.data_sync = cdc.FFSynchronizer(self.data.i, data_sync)
+        m.submodules.cs_buffer   = cs_buffer   = io.Buffer("i", self._ports.cs)
+        m.submodules.sck_buffer  = sck_buffer  = io.Buffer("i", self._ports.sck)
+        m.submodules.copi_buffer = copi_buffer = io.Buffer("i", self._ports.copi)
+        m.submodules.cipo_buffer = cipo_buffer = io.Buffer("i", self._ports.cipo)
 
-        m.submodules.cobs_encoder = encoder = COBSEncoder(fifo_depth=self._fifo_depth)
-        wiring.connect(m, wiring.flipped(self.stream), encoder.o)
+        if platform is not None:
+            # Works on iCE40HX8K.
+            platform.add_clock_constraint(sck_buffer.i, 100e6)
 
-        clk_past = Signal.like(clk_sync)
-        clk_edge = Signal()
-        m.d.sync += clk_past.eq(clk_sync)
-        m.d.comb += clk_edge.eq(clk_sync & ~clk_past)
+        # The analyzer frontend is clocked by SPI clock and resynchronized whenever CS# is
+        # asserted by using CS# as an active-high reset. This means that frequency of SCK is
+        # only limited by the FPGA fabric performance (compared to an approach that uses
+        # a sampling clock), but introduces timing hazards.
+        m.domains.spi = cd_spi = ClockDomain(async_reset=True, local=True)
+        m.submodules.spi_rst_sync = cdc.ResetSynchronizer(cs_buffer.i, domain="spi")
+        m.d.comb += cd_spi.clk.eq(sck_buffer.i)
 
-        idle  = Signal(20)
-        count = Signal(range(8))
-        shreg = Signal(8)
-        with m.FSM():
-            m.d.comb += encoder.i.p.data.eq(shreg)
+        # The FIFO contents must not be reset even if the FIFO writer is reset. This creates
+        # an unsynchronized CDC path between the FIFO writer and the FIFO, the practical effect
+        # of which is that there is a (difficult to specify) hold time constraint between CS#
+        # deassertion and preceding SCK edge. This timing hazard is an inherent feature of
+        # the approach taken in this component.
+        m.domains.fifo = cd_fifo = ClockDomain(reset_less=True, local=True)
+        m.d.comb += cd_fifo.clk.eq(sck_buffer.i)
 
-            with m.State("Idle"):
-                with m.If(cs_sync):
-                    m.d.sync += count.eq(0)
-                    m.next = "Capture"
-                with m.Else():
-                    with m.If(idle == 0):
-                        m.d.comb += self.flush.eq(1)
-                    with m.Else():
-                        m.d.sync += idle.eq(idle - 1)
+        m.submodules.fifo = fifo = StreamFIFO(
+            shape=data.StructLayout({
+                "copi": self._word_width,
+                "cipo": self._word_width,
+                "epoch": 1
+            }),
+            depth=4, # CDC only, no buffering
+            w_domain="fifo",
+            r_domain="sync"
+        )
 
-            with m.State("Capture"):
-                with m.If(clk_edge):
-                    m.d.sync += shreg.eq(Cat(data_sync, shreg))
-                    m.d.sync += count.eq(count + 1)
-                    with m.If(count == 7):
-                        m.d.sync += count.eq(0)
-                        m.next = "Submit Data"
-                with m.If(~cs_sync):
-                    m.next = "Submit End"
+        copi_shreg = Signal(self._word_width)
+        cipo_shreg = Signal(self._word_width)
+        m.d.fifo += copi_shreg.eq(Cat(copi_buffer.i, copi_shreg))
+        m.d.fifo += cipo_shreg.eq(Cat(cipo_buffer.i, cipo_shreg))
+        m.d.comb += fifo.w.p.copi.eq(Cat(copi_buffer.i, copi_shreg))
+        m.d.comb += fifo.w.p.cipo.eq(Cat(cipo_buffer.i, cipo_shreg))
 
-            with m.State("Submit Data"):
-                m.d.comb += encoder.i.p.end.eq(0)
-                m.d.comb += encoder.i.valid.eq(1)
-                with m.If(encoder.i.ready):
-                    m.next = "Capture"
-                with m.If(clk_edge | ~cs_sync):
-                    m.next = "Overflow"
+        start = Signal(init=1)
+        epoch = Signal(reset_less=True, init=1)
+        count = Signal(range(self._word_width), init=2) # 2 is the ResetSynchronizer latency
+        with m.If(count == self._word_width - 1):
+            m.d.comb += fifo.w.valid.eq(1)
+            with m.If(start):
+                m.d.spi += start.eq(0)
+                m.d.spi += epoch.eq(~epoch)
+                m.d.comb += fifo.w.p.epoch.eq(~epoch)
+            with m.Else():
+                m.d.comb += fifo.w.p.epoch.eq(epoch)
+            m.d.spi += count.eq(0)
+        with m.Else():
+            m.d.spi += count.eq(count + 1)
 
-            with m.State("Submit End"):
-                m.d.comb += encoder.i.p.end.eq(1)
-                m.d.comb += encoder.i.valid.eq(1)
-                with m.If(encoder.i.ready):
-                    m.d.sync += idle.eq(~0)
-                    m.next = "Idle"
-                with m.If(cs_sync):
-                    m.next = "Overflow"
+        overflow_spi  = Signal()
+        with m.If(fifo.w.valid & ~fifo.w.ready):
+            m.d.spi += overflow_spi.eq(1)
 
-            with m.State("Overflow"):
-                m.d.comb += self.flush.eq(1)
-                m.d.comb += self.overflow.eq(1)
+        wiring.connect(m, wiring.flipped(self.stream), fifo.r)
+
+        cs_sync = Signal()
+        # Note that the async FIFO write-to-read latency, and the latency of this synchronizer,
+        # are the same (2 cycles). This synchronizer isn't critical to the operation of
+        # the frontend; it is only used to avoid the last transfer being stuck in the encoder
+        # indefinitely because there is no end marker. Back-to-back transfers may not ever cause
+        # the `complete` output to be asserted.
+        m.submodules.cs_sync = cdc.FFSynchronizer(cs_buffer.i, cs_sync)
+        with m.If(cs_sync & ~fifo.r.valid):
+            m.d.comb += self.complete.eq(1)
+
+        overflow_sync = Signal()
+        m.submodules.overflow_sync = cdc.FFSynchronizer(overflow_spi, overflow_sync)
+        with m.If(overflow_sync):
+            m.d.sync += self.overflow.eq(1)
 
         return m
 
 
 class SPIAnalyzerComponent(wiring.Component):
-    copi_stream: Out(stream.Signature(8))
-    copi_flush:  Out(1)
-    cipo_stream: Out(stream.Signature(8))
-    cipo_flush:  Out(1)
+    o_stream: Out(stream.Signature(8))
+    o_flush:  Out(1)
 
     overflow: Out(1)
 
-    def __init__(self, ports, *, copi_fifo_depth, cipo_fifo_depth):
+    def __init__(self, ports):
         self.ports = ports
-
-        self._copi_fifo_depth = copi_fifo_depth
-        self._cipo_fifo_depth = cipo_fifo_depth
 
         super().__init__()
 
     def elaborate(self, platform):
         m = Module()
 
-        m.submodules.cs_buffer  = cs_buffer  = io.Buffer("i", ~self.ports.cs)
-        m.submodules.sck_buffer = sck_buffer = io.Buffer("i", self.ports.sck)
+        m.submodules.frontend = frontend = SPIAnalyzerFrontend(self.ports)
+        m.submodules.encoder  = encoder  = COBSEncoder(fifo_depth=512) # one BRAM on iCE40
 
-        if self.ports.copi:
-            m.submodules.copi_buffer = copi_buffer = io.Buffer("i", self.ports.copi)
-            m.submodules.copi_channel = copi_channel = \
-                SPIChannelAnalyzer(fifo_depth=self._copi_fifo_depth)
-            wiring.connect(m, copi_channel.cs,   cs_buffer)
-            wiring.connect(m, copi_channel.clk,  sck_buffer)
-            wiring.connect(m, copi_channel.data, copi_buffer)
-            wiring.connect(m, wiring.flipped(self.copi_stream), copi_channel.stream)
-            m.d.comb += self.copi_flush.eq(copi_channel.flush)
-            with m.If(copi_channel.overflow):
-                m.d.comb += self.overflow.eq(1)
+        wiring.connect(m, wiring.flipped(self.o_stream), encoder.o)
 
-        if self.ports.cipo:
-            m.submodules.cipo_buffer = cipo_buffer = io.Buffer("i", self.ports.cipo)
-            m.submodules.cipo_channel = cipo_channel = \
-                SPIChannelAnalyzer(fifo_depth=self._cipo_fifo_depth)
-            wiring.connect(m, cipo_channel.cs,   cs_buffer)
-            wiring.connect(m, cipo_channel.clk,  sck_buffer)
-            wiring.connect(m, cipo_channel.data, cipo_buffer)
-            wiring.connect(m, wiring.flipped(self.cipo_stream), cipo_channel.stream)
-            m.d.comb += self.cipo_flush.eq(cipo_channel.flush)
-            with m.If(cipo_channel.overflow):
-                m.d.comb += self.overflow.eq(1)
+        idle  = Signal(init=1)
+        epoch = Signal()
+        timer = Signal(20)
+        with m.FSM():
+            with m.State("COPI"):
+                with m.If(frontend.stream.valid):
+                    with m.If(frontend.stream.p.epoch != epoch):
+                        m.d.comb += encoder.i.p.end.eq(1)
+                        m.d.comb += encoder.i.valid.eq(1)
+                        with m.If(encoder.i.ready):
+                            m.d.sync += idle.eq(1)
+                            m.d.sync += epoch.eq(frontend.stream.p.epoch)
+                    with m.Else():
+                        m.d.comb += encoder.i.p.data.eq(frontend.stream.p.copi)
+                        m.d.comb += encoder.i.valid.eq(1)
+                        with m.If(encoder.i.ready):
+                            m.d.sync += idle.eq(0)
+                            # FIXME: not the most elegant approach to make the timeout shorter
+                            # during simulation
+                            m.d.sync += timer.eq(1000 if platform is None else ~0)
+                            m.next = "CIPO"
+                with m.Elif(frontend.complete & ~idle):
+                    m.d.comb += encoder.i.p.end.eq(1)
+                    m.d.comb += encoder.i.valid.eq(1)
+                    with m.If(encoder.i.ready):
+                        m.d.sync += idle.eq(1)
+                        m.d.sync += epoch.eq(~epoch)
+
+                # FIXME: this timeout should be a part of the common FX2 logic
+                with m.Else():
+                    with m.If(timer == 0):
+                        m.d.comb += self.o_flush.eq(1)
+                    with m.Else():
+                        m.d.sync += timer.eq(timer - 1)
+
+            with m.State("CIPO"):
+                m.d.comb += encoder.i.p.data.eq(frontend.stream.p.cipo)
+                m.d.comb += encoder.i.valid.eq(1)
+                with m.If(encoder.i.ready):
+                    m.d.comb += frontend.stream.ready.eq(1)
+                    m.next = "COPI"
+
+        m.d.comb += self.overflow.eq(frontend.overflow)
 
         return m
 
 
 class SPIAnalyzerInterface:
-    def __init__(self, logger, assembly, *, cs, sck, copi=None, cipo=None, fifo_depth=512):
+    def __init__(self, logger, assembly, *, cs, sck, copi, cipo):
         self._logger = logger
         self._level  = logging.DEBUG if self._logger.name == __name__ else logging.TRACE
 
         ports = assembly.add_port_group(cs=cs, sck=sck, copi=copi, cipo=cipo)
-        component = assembly.add_submodule(
-            SPIAnalyzerComponent(ports, copi_fifo_depth=fifo_depth, cipo_fifo_depth=fifo_depth))
-        # Use the smallest host interface FIFO, since the COBS encoder includes its own, which
-        # is more efficient resource-wise as it is combined with the lookahead buffer.
-        self._copi_pipe = assembly.add_in_pipe(
-            component.copi_stream, in_flush=component.copi_flush, fifo_depth=2)
-        self._cipo_pipe = assembly.add_in_pipe(
-            component.cipo_stream, in_flush=component.cipo_flush, fifo_depth=2)
+        component = assembly.add_submodule(SPIAnalyzerComponent(ports))
+        # Use only a minimal interface FIFO; most of the buffering is done in the COBS encoder.
+        self._pipe = assembly.add_in_pipe(
+            component.o_stream, in_flush=component.o_flush, fifo_depth=4)
         self._overflow = assembly.add_ro_register(component.overflow)
+
+        self._buffer  = bytearray()
+        self._packets = []
 
     def _log(self, message, *args):
         self._logger.log(self._level, "SPI analyzer: " + message, *args)
 
-    @staticmethod
-    async def _recv_cobs_packet(pipe) -> bytes:
-        buffer = bytearray()
-        while byte := await pipe.recv(1):
-            if byte == b"\x00":
-                return cobs_decode(buffer)
-            else:
-                buffer += byte
+    async def _recv_packet(self) -> bytes:
+        while not self._packets:
+            if self._pipe.readable == 0 and await self._overflow:
+                raise SPIAnalyzerOverflow("overflow")
+
+            self._buffer += await self._pipe.recv(self._pipe.readable or 1)
+            if b"\x00" in self._buffer:
+                *self._packets, self._buffer = self._buffer.split(b"\x00")
+
+        packet = self._packets[0]
+        del self._packets[0]
+        return cobs_decode(packet)
 
     async def capture(self) -> tuple[bytes, bytes]:
-        if await self._overflow:
-            raise SPIAnalyzerOverflow("overflow")
-        copi_data, cipo_data = await asyncio.gather(
-            self._recv_cobs_packet(self._copi_pipe),
-            self._recv_cobs_packet(self._cipo_pipe))
+        packet = await self._recv_packet()
+        copi_data, cipo_data = packet[0::2], packet[1::2]
         self._log("capture copi=<%s> cipo=<%s>", dump_hex(copi_data), dump_hex(cipo_data))
         return (copi_data, cipo_data)
 
@@ -192,10 +228,21 @@ class SPIAnalyzerInterface:
 class SPIAnalyzerApplet(GlasgowAppletV2):
     logger = logging.getLogger(__name__)
     help = "analyze SPI transactions"
-    preview = True
     description = """
     Capture transactions on the SPI bus.
+
+    SPI data is captured in a clock domain driven by the SCK pin and reset by the CS# pin.
+    This approach enables capturing data at very high SCK frequencies (up to 100 MHz), but
+    requires a small delay between the last SCK rising edge and CS# rising edge, otherwise
+    the last byte of a transaction will not be captured correctly. Typically, SPI controllers
+    do provide this delay.
+
+    Signal integrity is exceptionally important for this applet. When using flywires, twist
+    every signal wire (at the very least, CS# and SCK wires) with a ground wire connected to
+    ground at both ends, otherwise the captured data will likely be nonsense.
     """
+    # May work on revA/B with a looser clock constraint on SCK.
+    required_revision = "C0"
 
     @classmethod
     def add_build_arguments(cls, parser, access):
@@ -205,15 +252,15 @@ class SPIAnalyzerApplet(GlasgowAppletV2):
         access.add_pins_argument(parser, "copi", required=True, default=True)
         access.add_pins_argument(parser, "cipo", required=True, default=True)
 
-        parser.add_argument("--fifo-depth", metavar="BYTES", type=int, default=512,
-            help="use a hardware FIFO that is BYTES deep (default: %(default)s, up to: 8192)")
-
     def build(self, args):
         with self.assembly.add_applet(self):
             self.assembly.use_voltage(args.voltage)
+            self.assembly.use_pulls({
+                args.cs:   "high", args.sck:  "low",
+                args.copi: "high", args.cipo: "high"
+            })
             self.spi_analyzer_iface = SPIAnalyzerInterface(self.logger, self.assembly,
-                cs=args.cs, sck=args.sck, copi=args.copi, cipo=args.cipo,
-                fifo_depth=args.fifo_depth)
+                cs=args.cs, sck=args.sck, copi=args.copi, cipo=args.cipo)
 
     @classmethod
     def add_run_arguments(cls, parser):
