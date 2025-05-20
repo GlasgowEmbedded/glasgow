f4266465a9882b87af2490bcab21c81a30fc28c3
Catherine <whitequark@whitequark.org>
2025-05-20 10:52:17 +0000

    applet.interface.jtag_openocd: migrate to V2 API.

--- software/glasgow/applet/interface/jtag_openocd/test.py
+++ software/glasgow/applet/interface/jtag_openocd/test.py
@@ -1,8 +1,12 @@
-from ... import *
+from glasgow.applet import GlasgowAppletV2TestCase, synthesis_test
 from . import JTAGOpenOCDApplet
 
 
-class JTAGOpenOCDAppletTestCase(GlasgowAppletTestCase, applet=JTAGOpenOCDApplet):
+class JTAGOpenOCDAppletTestCase(GlasgowAppletV2TestCase, applet=JTAGOpenOCDApplet):
     @synthesis_test
     def test_build(self):
         self.assertBuilds()
+
+    @synthesis_test
+    def test_build_trst_srst(self):
+        self.assertBuilds("--trst A6 --srst A7")
