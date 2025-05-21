6bde7880f13d05f0385ff9a885d57803b22cf35c
Wanda <wanda@phinode.net>
2025-05-21 13:48:14 +0100

    applet.interface.jtag_pinout: migrate to V2 API.

--- software/glasgow/applet/interface/jtag_pinout/test.py
+++ software/glasgow/applet/interface/jtag_pinout/test.py
@@ -1,8 +1,8 @@
-from ... import *
+from glasgow.applet import GlasgowAppletV2TestCase, synthesis_test
 from . import JTAGPinoutApplet
 
 
-class JTAGPinoutAppletTestCase(GlasgowAppletTestCase, applet=JTAGPinoutApplet):
+class JTAGPinoutAppletTestCase(GlasgowAppletV2TestCase, applet=JTAGPinoutApplet):
     @synthesis_test
     def test_build(self):
         self.assertBuilds(args=["--pins", "A0:3"])
