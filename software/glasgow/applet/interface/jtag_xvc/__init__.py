GlasgowEmbedded/glasgow

commit 201703dd723ae90d98a836cff814dfdc10d3efc1

applet.interface.jtag_xvc: allow overriding frequency.
Vivado no longer allows doing this from the GUI, and picks a rather
high frequency by default.

software/glasgow/applet/interface/jtag_xvc/__init__.py
@@ -219,7 +219,14 @@ class JTAGXVCApplet(GlasgowAppletV2):
     def add_run_arguments(cls, parser):
         ServerEndpoint.add_argument(parser, "endpoint", default="tcp:localhost:2542")
 
+        parser.add_argument(
+            "-f", "--frequency", metavar="FREQ", type=int, default=1000,
+            help="override TCK frequency to always be FREQ kHz (default: %(default)s)")
+
     async def run(self, args):
+        if args.frequency:
+            await self.xvc_iface.set_tck_period(round(1e6 / args.frequency))
+
         endpoint = await ServerEndpoint("socket", self.logger, args.endpoint)
         while True:
             command = await endpoint.recv_until(b":")
@@ -231,7 +238,8 @@ class JTAGXVCApplet(GlasgowAppletV2):
                 case b"settck":
                     tck_period, = struct.unpack("<L", await endpoint.recv(4))
                     self.logger.debug(f"  tck-i={tck_period}")
-                    await self.xvc_iface.set_tck_period(tck_period)
+                    if not args.frequency:
+                        await self.xvc_iface.set_tck_period(tck_period)
                     tck_period = round(await self.xvc_iface.get_tck_period())
                     self.logger.debug(f"  tck-o={tck_period}")
                     await endpoint.send(struct.pack("<L", tck_period))

commit 3ed6f3bcf98bfc1a0e9a9c7c181430fa2a61f2a7

applet.interface.jtag_xvc: fix `--frequency` option.
Bug introduced in 201703dd.

software/glasgow/applet/interface/jtag_xvc/__init__.py
@@ -220,11 +220,11 @@ class JTAGXVCApplet(GlasgowAppletV2):
         ServerEndpoint.add_argument(parser, "endpoint", default="tcp:localhost:2542")
 
         parser.add_argument(
-            "-f", "--frequency", metavar="FREQ", type=int, default=1000,
+            "-f", "--frequency", metavar="FREQ", type=int, default=None,
             help="override TCK frequency to always be FREQ kHz (default: %(default)s)")
 
     async def run(self, args):
-        if args.frequency:
+        if args.frequency is not None:
             await self.xvc_iface.set_tck_period(round(1e6 / args.frequency))
 
         endpoint = await ServerEndpoint("socket", self.logger, args.endpoint)
@@ -238,7 +238,7 @@ class JTAGXVCApplet(GlasgowAppletV2):
                 case b"settck":
                     tck_period, = struct.unpack("<L", await endpoint.recv(4))
                     self.logger.debug(f"  tck-i={tck_period}")
-                    if not args.frequency:
+                    if args.frequency is None:
                         await self.xvc_iface.set_tck_period(tck_period)
                     tck_period = round(await self.xvc_iface.get_tck_period())
                     self.logger.debug(f"  tck-o={tck_period}")