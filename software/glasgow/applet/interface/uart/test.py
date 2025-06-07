90b64a5ac77bde5847b72089a9d4af9ec31a7b5b
Thomas Watson <twatson52@icloud.com>
2025-06-07 15:59:00 -0500

    applet.interface.uart: add parity loopback test

--- software/glasgow/applet/interface/uart/test.py
+++ software/glasgow/applet/interface/uart/test.py
@@ -17,6 +17,11 @@ class UARTAppletTestCase(GlasgowAppletV2TestCase, applet=UARTApplet):
         await applet.uart_iface.write(bytes([0xAA, 0x55]))
         self.assertEqual(await applet.uart_iface.read(2), bytes([0xAA, 0x55]))
 
+    @applet_v2_simulation_test(prepare=prepare_loopback, args="--baud 9600 --parity odd")
+    async def test_parity_loopback(self, applet, ctx):
+        await applet.uart_iface.write(bytes([0x55, 0xAA]))
+        self.assertEqual(await applet.uart_iface.read(2), bytes([0x55, 0xAA]))
+
     # This test is here mainly to test the test machinery.
     @applet_v2_hardware_test(args="-V 3.3 --baud 9600", mock="uart_iface")
     async def test_loopback_hw(self, applet):
