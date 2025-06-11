ba4c7619a5bb8fa418ca5bffeb3afc1ed41ae5e2
Catherine <whitequark@whitequark.org>
2025-06-11 16:09:49 +0000

    arch.arm.dap: add MEM-AP registers.

--- software/glasgow/arch/arm/dap/ap.py
+++ software/glasgow/arch/arm/dap/ap.py
@@ -9,10 +9,13 @@ from ....support.bitstruct import *
 
 __all__ = [
     "AP_IDR_addr", "AP_IDR", "AP_IDR_CLASS",
+    "MEM_AP_CSW_addr", "MEM_AP_CSW", "MEM_AP_TAR_addr", "MEM_AP_DRW_addr", "MEM_AP_BD_addr",
+    "MEM_AP_CFG_addr", "MEM_AP_CFG", "MEM_AP_CFG1_addr", "MEM_AP_CFG1",
+    "MEM_AP_BASE_addr", "MEM_AP_BASE",
 ]
 
 
-# IDR AP register layout
+# Generic AP register layout
 
 AP_IDR_addr = 0xFC
 
@@ -41,3 +44,57 @@ class AP_IDR_CLASS(Enum):
                 return "MEM-AP"
             case _:
                 return f"{self.value:#06b}"
+
+
+# MEM-AP register layout
+
+MEM_AP_CSW_addr = 0x00
+
+MEM_AP_CSW = bitstruct("MEM_AP_CSW", 32, [
+    ("Size",        3),
+    (None,          1),
+    ("AddrInc",     2),
+    ("DeviceEn",    1),
+    ("TrInProg",    1),
+    ("Mode",        4),
+    ("Type",        3),
+    ("MTE",         1),
+    (None,          7),
+    ("SPIDEN",      1),
+    (None,          7),
+    ("DbgSwEnable", 1),
+])
+
+MEM_AP_TAR_addr = 0x04
+
+MEM_AP_DRW_addr = 0x0C
+
+def MEM_AP_BD_addr(index: int):
+    assert index in range(4)
+    return 0x10 + (index << 2)
+
+MEM_AP_CFG_addr = 0xF4
+
+MEM_AP_CFG = bitstruct("MEM_AP_CFG", 32, [
+    ("BE",          1),
+    ("LA",          1),
+    ("LD",          1),
+    (None,          29),
+])
+
+MEM_AP_CFG1_addr = 0xE0
+
+MEM_AP_CFG1 = bitstruct("MEM_AP_CFG1", 32, [
+    ("TAG0SIZE",    4),
+    ("TAG0GRAN",    4),
+    (None,          24),
+])
+
+MEM_AP_BASE_addr = 0xF8
+
+MEM_AP_BASE = bitstruct("MEM_AP_BASE", 32, [
+    ("P",           1),
+    ("Format",      1),
+    (None,          14),
+    ("BASEADDR",    16),
+])
