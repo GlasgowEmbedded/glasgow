1078f201a2ab0f61108cdfe1bb7d3bb6d2b7efe9
Catherine <whitequark@whitequark.org>
2025-07-03 23:32:44 +0000

    applet.program_ice40_flash: modernize use of GPIO.

--- software/glasgow/applet/program/ice40_flash/test.py
+++ software/glasgow/applet/program/ice40_flash/test.py
@@ -4,5 +4,9 @@ from . import ProgramICE40FlashApplet
 
 class ProgramICE40FlashAppletTestCase(GlasgowAppletV2TestCase, applet=ProgramICE40FlashApplet):
     @synthesis_test
-    def test_build(self):
-        self.assertBuilds("--reset B0 --done B1")
+    def test_build_with_done(self):
+        self.assertBuilds(args=["--done", "A7"])
+
+    @synthesis_test
+    def test_build_without_done(self):
+        self.assertBuilds(args=["--done", "-"])
