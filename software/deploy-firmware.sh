4e98b6cc9380baf75db54de23b4dbc9fbba4ff52
Andrei Purdea <andrei@purdea.ro>
2025-10-22 07:51:00 +0300

    firmware: Add build script to more easily use docker for development.
    Added "./docker.sh" build script with various subcommands in order to
    encourage using the official sdcc version even for experimental
    developer builds of the firmware. They will work much faster than the
    previous `deploy-firmware.sh` script because they now use docker's
    caching mechanism to not reinstall sdcc every time they are invoked.
    
    One of the subcommands `./docker.sh deploy` now replaces the old
    `deploy-firmware.sh` script. The console output will look a little bit
    different, because the installation steps are now printed by
    `docker build --progress=plain`. The caching mechanism is disabled
    only for deploying.
    
    This also introduces the following changes to deploy-firmware:
    - Get rid of the warning produced by apt-get by setting DEBIAN_FRONTEND
    - Primary group id is now determined with the `id -g` command
    - User id is now determined with the `id -u` command

--- software/deploy-firmware.sh
+++ software/deploy-firmware.sh
@@ -1,56 +1,6 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# This script is provided to ensure builds of firmware included in the repository are byte for byte
-# reproducible regardless of the host OS or the packages installed.
-#
-# Using this script is not a requirement to work on the firmware; installation instructions for
-# the prerequisites for a selection of operating systems are included in the documentation.
+set -e
 
-BASE_IMAGE=debian:trixie-20250721-slim@sha256:cc92da07b99dd5c078cb5583fdb4ba639c7c9c14eb78508a2be285ca67cc738a
-
-if [ -z "${DOCKER}" ]; then
-    exec docker run \
-        --volume $(dirname $(dirname $(readlink -f $0))):/glasgow \
-        --workdir /glasgow \
-        --env DOCKER=1 \
-        --env UID=$(id -u) \
-        --env GID=$(id -g) \
-        --rm ${BASE_IMAGE} \
-        software/deploy-firmware.sh
-fi
-
-set -ex
-
-# Install dependencies.
-apt-get update -qq
-apt-get install -qq --no-install-recommends git make sdcc python3 python3-usb1
-
-# Any commands that create new files in the host mount must be invoked with the caller UID/GID, or
-# else the created files will be owned by root. We can't use `docker run --user` because then
-# apt-get would not be able to install packages.
-#
-# Create a user and a group with the UID/GID of the caller.
-groupadd --gid ${GID} caller || true
-useradd --uid ${UID} --gid ${GID} caller
-
-# Do the work.
-su caller - <<END
-
-# Display dependency versions.
-sdcc --version
-
-# Clean all build products; they may have been built using a different compiler.
-make -C vendor/libfx2/firmware/library clean
-make -C firmware clean
-
-# Build the artifact.
-make -C vendor/libfx2/firmware/library all MODELS=medium
-make -C firmware all
-
-# Deploy the artifact. For incomprehensible (literally; I could not figure out why) reasons,
-# the Debian and NixOS builds of exact same commit of sdcc produce different .ihex files that
-# nevertheless translate to the same binary contents.
-PYTHONPATH=vendor/libfx2/software python3 firmware/normalize.py \
-    firmware/glasgow.ihex software/glasgow/hardware/firmware.ihex
-
-END
+cd $(dirname $(dirname $(readlink -f $0)))/firmware
+./docker.sh deploy
