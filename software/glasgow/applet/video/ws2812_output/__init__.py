26eb7e78583159323f5759cb7e393e43a95b1661
Balint Kovacs <dratini0@gmail.com>
2025-12-06 22:17:24 +0000

    applet.video.ws2812_output: Improve interface class usability

--- software/glasgow/applet/video/ws2812_output/__init__.py
+++ software/glasgow/applet/video/ws2812_output/__init__.py
@@ -1,4 +1,6 @@
+from dataclasses import dataclass
 import logging
+import typing as t
 
 from amaranth import *
 from amaranth.lib import io, wiring, stream
@@ -8,6 +10,37 @@ from glasgow.abstract import AbstractAssembly, ClockDivisor, GlasgowPin, PortGro
 from glasgow.applet import GlasgowAppletV2
 from glasgow.support.endpoint import *
 from glasgow.gateware.pll import *
+
+
+__all__ = [
+    "VideoWS2812PixelFormat",
+    "VIDEO_WS2812_PIXEL_FORMATS",
+    "VideoWS2812OutputComponent",
+    "VideoWS2812OutputInterface",
+]
+
+
+@dataclass(frozen=True)
+class VideoWS2812PixelFormat:
+    in_size: int
+    out_size: int
+    format_func: t.Callable
+
+
+VIDEO_WS2812_PIXEL_FORMATS = {
+    "RGB-BRG": VideoWS2812PixelFormat(
+        in_size=3, out_size=3, format_func=lambda r, g, b: Cat(b, r, g)
+    ),
+    "RGB-BGR": VideoWS2812PixelFormat(
+        in_size=3, out_size=3, format_func=lambda r, g, b: Cat(b, g, r)
+    ),
+    "RGB-xBRG": VideoWS2812PixelFormat(
+        in_size=3, out_size=4, format_func=lambda r, g, b: Cat(Const(0, unsigned(8)), b, r, g)
+    ),
+    "RGBW-WBRG": VideoWS2812PixelFormat(
+        in_size=4, out_size=4, format_func=lambda r, g, b, w: Cat(w, b, r, g)
+    ),
+}
 
 
 class VideoWS2812Output(Elaboratable):
@@ -28,14 +61,10 @@ class VideoWS2812OutputComponent(wiring.Component):
     i_stream: In(stream.Signature(8))
     framerate_divisor: In(24)
 
-    def __init__(
-        self, ports: PortGroup, count: int, pix_in_size: int, pix_out_size: int, pix_format_func
-    ):
+    def __init__(self, ports: PortGroup, count: int, pixel_format: VideoWS2812PixelFormat):
         self.ports = ports
         self.count = count
-        self.pix_in_size = pix_in_size
-        self.pix_out_size = pix_out_size
-        self.pix_format_func = pix_format_func
+        self.pixel_format = pixel_format
 
         super().__init__()
 
@@ -58,8 +87,8 @@ class VideoWS2812OutputComponent(wiring.Component):
 
         m.submodules.output = output = VideoWS2812Output(self.ports)
 
-        pix_in_size = self.pix_in_size
-        pix_out_size = self.pix_out_size
+        pix_in_size = self.pixel_format.in_size
+        pix_out_size = self.pixel_format.out_size
         pix_out_bpp = pix_out_size * 8
 
         cyc_ctr = Signal(range(t_reset + 1))
@@ -88,7 +117,7 @@ class VideoWS2812OutputComponent(wiring.Component):
                             byt_ctr.eq(byt_ctr + 1),
                         ]
                     with m.Else():
-                        p = self.pix_format_func(*pix, self.i_stream.payload)
+                        p = self.pixel_format.format_func(*pix, self.i_stream.payload)
                         m.d.sync += word.eq(Cat(word[pix_out_bpp:], p))
                         with m.If(word_ctr < (len(self.ports.out) - 1)):
                             m.d.sync += [
@@ -159,19 +188,15 @@ class VideoWS2812OutputInterface:
         *,
         out: tuple[GlasgowPin],
         count: int,
-        pix_in_size: int,
-        pix_out_size: int,
-        pix_format_func,
+        pixel_format: VideoWS2812PixelFormat,
         buffer: int,
     ):
         self._logger = logger
-        self._frame_size = len(out) * pix_in_size * count
+        self._frame_size = len(out) * pixel_format.in_size * count
         ports = assembly.add_port_group(out=out)
-        component = assembly.add_submodule(
-            VideoWS2812OutputComponent(ports, count, pix_in_size, pix_out_size, pix_format_func)
-        )
+        component = assembly.add_submodule(VideoWS2812OutputComponent(ports, count, pixel_format))
         self._pipe = assembly.add_out_pipe(
-            component.i_stream, buffer_size=len(out) * count * pix_in_size * buffer
+            component.i_stream, buffer_size=self._frame_size * buffer
         )
         self._framerate = assembly.add_clock_divisor(
             component.framerate_divisor, ref_period=assembly.sys_clk_period, name="framerate"
@@ -201,14 +226,6 @@ class VideoWS2812OutputApplet(GlasgowAppletV2):
     Output RGB(W) frames from a socket to one or more WS2812(B) LED strings.
     """
 
-    pixel_formats = {
-        # in-out      in size  out size  format_func
-        "RGB-BRG":   (   3,        3,    lambda r,g,b:   Cat(b,r,g)   ),
-        "RGB-BGR":   (   3,        3,    lambda r,g,b:   Cat(b,g,r)   ),
-        "RGB-xBRG":  (   3,        4,    lambda r,g,b:   Cat(Const(0, unsigned(8)),b,r,g) ),
-        "RGBW-WBRG": (   4,        4,    lambda r,g,b,w: Cat(w,b,r,g) ),
-    }
-
     @classmethod
     def add_build_arguments(cls, parser, access):
         access.add_voltage_argument(parser)
@@ -217,15 +234,13 @@ class VideoWS2812OutputApplet(GlasgowAppletV2):
             "-c", "--count", metavar="N", type=int, required=True,
             help="set the number of LEDs per string")
         parser.add_argument(
-            "-f", "--pix-fmt", metavar="F", choices=cls.pixel_formats.keys(), default="RGB-BRG",
-            help="set the pixel format (default: %(default)s)")
+            "-f", "--pix-fmt", metavar="F", choices=VIDEO_WS2812_PIXEL_FORMATS.keys(),
+            default="RGB-BRG", help="set the pixel format (default: %(default)s)")
         parser.add_argument(
             "-b", "--buffer", metavar="N", type=int, default=16,
             help="set the number of frames to buffer internally (buffered twice)")
 
     def build(self, args):
-        self.pix_in_size, pix_out_size, pix_format_func = self.pixel_formats[args.pix_fmt]
-
         with self.assembly.add_applet(self):
             self.assembly.use_voltage(args.voltage)
             self.ws2812_iface = VideoWS2812OutputInterface(
@@ -233,9 +248,7 @@ class VideoWS2812OutputApplet(GlasgowAppletV2):
                 self.assembly,
                 out=args.out,
                 count=args.count,
-                pix_in_size=self.pix_in_size,
-                pix_out_size=pix_out_size,
-                pix_format_func=pix_format_func,
+                pixel_format=VIDEO_WS2812_PIXEL_FORMATS[args.pix_fmt],
                 buffer=args.buffer,
             )
 
