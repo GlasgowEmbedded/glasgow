e26e66f18a46939ba40e820588a4a581a5123993
sazzach <sazzach@gmail.com>
2026-01-24 21:25:15 -0700

    applet.sensor.hx711: migrate to V2 API

--- software/glasgow/applet/sensor/hx711/__init__.py
+++ software/glasgow/applet/sensor/hx711/__init__.py
@@ -2,131 +2,302 @@
 # Accession: G00049
 
 import logging
-from amaranth import *
-from amaranth.lib import io, cdc
+from typing import Literal
 
-from ... import *
-from ....gateware.clockgen import *
-from ....support.data_logger import DataLogger
+from amaranth import *
+from amaranth.lib import enum, wiring, io, cdc, stream
+from amaranth.lib.wiring import In, Out
+
+from glasgow.abstract import AbstractAssembly, ClockDivisor, GlasgowPin
+from glasgow.applet import GlasgowAppletV2, GlasgowAppletError
+from glasgow.support.data_logger import DataLogger
+
+
+__all__ = ["SensorHX711Interface", "SensorHX711Controller", "SensorHX711Component",
+           "HX711Setting", "HX711Error"]
 
 
 class HX711Error(GlasgowAppletError):
     pass
 
 
-class HX711Bus(Elaboratable):
-    def __init__(self, ports):
-        self.ports = ports
-        self.clk = Signal()
-        self.din = Signal()
-        self.osc = Signal()
+class HX711Setting(enum.IntEnum):
+    """HX711Setting is an Enum that represents the three valid combinations of channel and gain."""
+
+    A_128 = 25
+    "A, 128 is the default (reset) value of channel and gain."
+    B_32  = 26
+    A_64  = 27
+
+    @classmethod
+    def from_channel_gain(
+        cls, channel: Literal["A", "B"], gain: Literal[32, 64, 128]
+    ) -> "HX711Setting":
+        match (channel, gain):
+            case ("B", 32):
+                return cls.B_32
+            case ("A", 64):
+                return cls.A_64
+            case ("A", 128):
+                return cls.A_128
+            case _:
+                raise ValueError(
+                    f"HX711 does not support a combination of channel {channel} and gain {gain}"
+                )
+
+
+class SensorHX711Controller(wiring.Component):
+    sck_divisor: In(16)
+    """Serial Clock (sck) clock divisor.
+
+    sck frequency is freq(sync) / (2*(sck_divisor+1))
+    """
+
+    osc_divisor: In(16)
+    """Reference frequency clock divisor.
+
+    osc frequency is freq(sync) / (2*(sck_divisor+1))
+    """
+
+    control_stream: In(stream.Signature(HX711Setting))
+    """A transfer instructs :py:`SensorHX711Controller` to perform a bus transaction.
+
+    The :py:`payload` is an :py:`Enum` representing one of the three valid combinations of channel
+    and gain.
+
+    A bus transaction reads the current sample, and additionally, configures the channel and gain
+    for the following sample. :py:`ready` will not be reasserted until both the bus transaction is
+    complete and the acquired sample is transferred out on :py:`sample_stream`
+    """
+
+    sample_stream: Out(stream.Signature(Shape(24, signed=True)))
+    """Stream for samples read from the HX711
+
+    The :py:`payload` is a signed 24 bit integer representing the raw value of the sample.
+    """
+
+    def __init__(self, sck: io.PortLike, din: io.PortLike, osc: io.PortLike | None):
+        self._sck = sck
+        self._din = din
+        self._osc = osc
+        super().__init__()
 
     def elaborate(self, platform):
         m = Module()
-        m.submodules.clk_buffer = clk_buffer = io.Buffer("o", self.ports.clk)
-        m.d.comb += clk_buffer.o.eq(self.clk)
-        m.submodules.din_buffer = din_buffer = io.Buffer("i", self.ports.din)
-        m.submodules += cdc.FFSynchronizer(din_buffer.i, self.din)
-        if self.ports.osc is not None:
-            m.submodules.osc_buffer = osc_buffer = io.Buffer("o", self.ports.osc)
-            m.d.comb += osc_buffer.o.eq(self.osc)
-        return m
 
+        if self._osc is not None:
+            osc = Signal()
+            m.submodules.osc_buffer = osc_buffer = io.Buffer("o", self._osc)
+            m.d.comb += osc_buffer.o.eq(osc)
 
-class SensorHX711Subtarget(Elaboratable):
-    def __init__(self, ports, in_fifo, out_fifo, clk_cyc, osc_cyc):
-        self.ports    = ports
-        self.in_fifo  = in_fifo
-        self.out_fifo = out_fifo
-        self.clk_cyc  = clk_cyc
-        self.osc_cyc  = osc_cyc
+            osc_timer = Signal.like(self.osc_divisor)
+            with m.If(osc_timer == 0):
+                m.d.sync += [
+                    osc_timer.eq(self.osc_divisor),
+                    osc.eq(~osc)
+                ]
+            with m.Else():
+                m.d.sync += osc_timer.eq(osc_timer-1)
 
-    def elaborate(self, platform):
-        m = Module()
-        m.submodules.bus = bus = HX711Bus(self.ports)
+        sck = Signal()
+        din = Signal()
+        m.submodules.sck_buffer = sck_buffer = io.Buffer("o", self._sck)
+        m.submodules.din_buffer = din_buffer = io.Buffer("i", self._din)
+        m.d.comb += sck_buffer.o.eq(sck)
+        m.submodules += cdc.FFSynchronizer(din_buffer.i, din)
 
-        if self.osc_cyc is not None:
-            m.submodules.clkgen = clkgen = ClockGen(self.osc_cyc)
-            m.d.comb += bus.osc.eq(clkgen.clk)
+        sck_timer = Signal.like(self.sck_divisor)
+        limit = Signal(HX711Setting)
+        count = Signal(HX711Setting)
+        sample_shreg = Signal.like(self.sample_stream.payload)
+
+        m.d.comb += self.sample_stream.payload.eq(sample_shreg)
+        with m.If(self.sample_stream.ready):
+            m.d.sync += self.sample_stream.valid.eq(0)
 
         with m.FSM():
-            timer = Signal(range(self.clk_cyc))
-            count = Signal(range(28))
-            limit = Signal(range(28))
-            shreg = Signal(8)
-
             with m.State("IDLE"):
-                with m.If(self.out_fifo.r_rdy):
-                    m.d.comb += self.out_fifo.r_en.eq(1)
+                with m.If(self.control_stream.valid):
+                    m.d.comb += self.control_stream.ready.eq(1)
                     m.d.sync += [
-                        limit.eq(self.out_fifo.r_data),
+                        limit.eq(self.control_stream.payload),
                         count.eq(0),
                     ]
                     m.next = "WAIT"
 
             with m.State("WAIT"):
-                with m.If(~bus.din):
+                with m.If(~din):
+                    m.d.sync += sck_timer.eq(self.sck_divisor)
                     m.next = "SHIFT"
 
             with m.State("SHIFT"):
-                with m.If(timer != 0):
-                    m.d.sync += timer.eq(timer - 1)
+                with m.If(sck_timer != 0):
+                    m.d.sync += sck_timer.eq(sck_timer-1)
                 with m.Else():
                     m.d.sync += [
-                        timer.eq(self.clk_cyc),
-                        bus.clk.eq(~bus.clk),
+                        sck_timer.eq(self.sck_divisor),
+                        sck.eq(~sck),
                     ]
-                    with m.If(~bus.clk): # posedge
-                        m.d.sync += count.eq(count + 1)
-                        with m.If((count != 0) & (count % 8 == 0)):
-                            m.d.comb += [
-                                self.in_fifo.w_data.eq(shreg),
-                                self.in_fifo.w_en.eq(1),
-                            ]
-                    with m.If(bus.clk): # negedge
-                        m.d.sync += shreg.eq(Cat(bus.din, shreg))
-                        with m.If(count == limit):
-                            m.next = "IDLE"
+                    with m.If(~sck):
+                        # posedge
+                        m.d.sync += count.eq(count+1)
+
+                    with m.If(sck):
+                        # negedge
+                        with m.If(count <= 24):
+                            m.d.sync += sample_shreg.eq(Cat(din, sample_shreg))
+
+                        with m.If(count == 24):
+                            m.d.sync += self.sample_stream.valid.eq(1)
+
+                        with m.Elif(count >= limit):
+                            m.next = "FINISH-XFER"
+
+            with m.State("FINISH-XFER"):
+                with m.If(~self.sample_stream.valid):
+                    m.next = "IDLE"
 
         return m
 
 
-class HX711Interface:
-    def __init__(self, interface, logger):
-        self._lower  = interface
+class SensorHX711Component(wiring.Component):
+    i_stream: In(stream.Signature(8))
+    o_stream: Out(stream.Signature(8))
+
+    sck_divisor: In(16)
+    osc_divisor: In(16)
+
+    def __init__(self, ports):
+        self._ports = ports
+        super().__init__()
+
+    def elaborate(self, platform):
+        m = Module()
+
+        m.submodules.hx711_controller = hx711_controller = SensorHX711Controller(
+            sck=self._ports.sck, din=self._ports.din, osc=self._ports.osc)
+
+        m.d.comb += [
+            hx711_controller.sck_divisor.eq(self.sck_divisor),
+            hx711_controller.osc_divisor.eq(self.osc_divisor)
+        ]
+
+        i_stream_cast = stream.Signature(HX711Setting).create()
+        i_stream_cast.valid   = self.i_stream.valid
+        i_stream_cast.ready   = self.i_stream.ready
+        m.d.comb += i_stream_cast.p.eq(self.i_stream.p[0 : Shape.cast(HX711Setting).width])
+        wiring.connect(m, i_stream_cast, hx711_controller.control_stream)
+
+        with m.FSM():
+            word_count = Signal(range(3))
+            sample = Signal.like(hx711_controller.sample_stream.payload)
+
+            with m.State("IDLE"):
+                with m.If(hx711_controller.sample_stream.valid):
+                    m.d.comb += hx711_controller.sample_stream.ready.eq(1)
+                    m.d.sync += [
+                        sample.eq(hx711_controller.sample_stream.payload),
+                        word_count.eq(2),
+                    ]
+                    m.next = "SEND"
+
+            with m.State("SEND"):
+                m.d.comb += [
+                    self.o_stream.valid.eq(1),
+                    self.o_stream.payload.eq(sample.word_select(word_count, 8))
+                ]
+                with m.If(self.o_stream.ready):
+                    with m.If(word_count != 0):
+                        m.d.sync += word_count.eq(word_count - 1)
+                    with m.Else():
+                        m.next = "IDLE"
+
+        return m
+
+
+class SensorHX711Interface:
+    def __init__(self, logger: logging.Logger, assembly: AbstractAssembly, *,
+                 sck: GlasgowPin, din: GlasgowPin, osc: GlasgowPin | None = None):
         self._logger = logger
         self._level  = logging.DEBUG if self._logger.name == __name__ else logging.TRACE
 
-        self._channel = "A"
-        self._gain    = 128
+        self._setting = HX711Setting.A_128
+
+        component = assembly.add_submodule(
+            SensorHX711Component(assembly.add_port_group(sck=sck, din=din, osc=osc)))
+
+        self._pipe = assembly.add_inout_pipe(component.o_stream, component.i_stream)
+
+        self._sck_clock = assembly.add_clock_divisor(
+            component.sck_divisor, ref_period=assembly.sys_clk_period*2, name="sck")
+
+        if osc is not None:
+            self._osc_clock = assembly.add_clock_divisor(
+                component.osc_divisor, ref_period=assembly.sys_clk_period*2, name="osc")
+        else:
+            self._osc_clock = None
 
     def _log(self, message, *args):
         self._logger.log(self._level, "HX711: " + message, *args)
 
-    async def set_channel_gain(self, channel, gain):
-        if (channel, gain) not in (("A", 128), ("B", 32), ("A", 64)):
-            raise HX711Error(
-                f"HX711 does not support a combination of channel {channel} and gain {gain}")
-        self._channel = channel
-        self._gain    = gain
-        self._log("set channel=%s gain=%d", channel, gain)
-        await self.sample() # dummy sample to prime the settings
+    @property
+    def sck_clock(self) -> ClockDivisor:
+        """Serial Clock (sck) clock divisor.
 
-    async def sample(self):
-        channel_gain = (self._channel, self._gain)
-        if channel_gain == ("A", 128):
-            await self._lower.write([25])
-        elif channel_gain == ("B", 32):
-            await self._lower.write([26])
-        elif channel_gain == ("A", 64):
-            await self._lower.write([27])
+        The recommended frequency is 1MHz, otherwise the valid range is 20kHz to 5MHz.
+        """
+        return self._sck_clock
+
+    @property
+    def osc_clock(self) -> ClockDivisor:
+        """Reference frequency clock divisor.
+
+        The sample rate is either freq/1,105,920 or freq/138,240, depending on the HX711 rate pin.
+
+        Raises
+        ------
+        HX711Error
+            If the optional reference clock is not used and :py:`osc_clock` is accessed.
+        """
+        if self._osc_clock is not None:
+            return self._osc_clock
         else:
-            assert False
-        sample_as_unsigned = int.from_bytes(await self._lower.read(3), byteorder="big")
+            raise HX711Error(f"There is no pin assigned for osc.")
+
+    async def set_channel_gain(self, channel: Literal["A", "B"], gain: Literal[32, 64, 128]):
+        """Select the :py:`channel` to be sampled, and the :py:`gain` for it.
+
+        The valid combinations of :py:`channel` and :py:`gain` are:
+
+            "A", 128; "B", 32; "A", 64
+
+        :meth:`set_channel_gain` also performs a dummy sample to prime the
+        settings for following calls to :meth:`sample`
+
+        Raises
+        ------
+        ValueError
+            If an invalid combination of channel and gain is used.
+        """
+        self._setting = HX711Setting.from_channel_gain(channel, gain)
+        self._log("set channel=%s gain=%d", channel, gain)
+        # dummy sample to prime the settings
+        await self.sample()
+
+    async def sample(self) -> int:
+        """Read the current sample, and initiate the conversion of the next sample.
+
+        The full scale range is 2.56V / :py:`gain`.
+
+        Returns the sample as a signed 24 bit integer.
+        """
+        await self._pipe.send(self._setting.value.to_bytes())
+        await self._pipe.flush()
+        sample_as_unsigned = int.from_bytes(await self._pipe.recv(3), byteorder="big")
         return sample_as_unsigned - ((sample_as_unsigned & 0x800000) << 1)
 
 
-class SensorHX711Applet(GlasgowApplet):
+class SensorHX711Applet(GlasgowAppletV2):
     logger = logging.getLogger(__name__)
     help = "measure voltage with AVIA Semiconductor HX711"
     description = """
@@ -140,45 +311,40 @@ class SensorHX711Applet(GlasgowApplet):
 
     @classmethod
     def add_build_arguments(cls, parser, access):
-        super().add_build_arguments(parser, access)
+        access.add_voltage_argument(parser)
 
-        parser.add_argument(
-            "-f", "--frequency", metavar="FREQ", type=float,
-            help="set oscillator frequency to FREQ MHz")
-
-        access.add_pins_argument(parser, "clk", default=True)
-        access.add_pins_argument(parser, "din", default=True)
+        access.add_pins_argument(parser, "sck", default=True, required=True)
+        access.add_pins_argument(parser, "din", default=True, required=True)
         access.add_pins_argument(parser, "osc", required=False)
 
-    def build(self, target, args):
-        if args.frequency is None:
-            osc_cyc = None
-        else:
-            osc_cyc = self.derive_clock(clock_name="osc",
-                input_hz=target.sys_clk_freq, output_hz=args.frequency * 1e6)
+    def build(self, args):
+        with self.assembly.add_applet(self):
+            self.assembly.use_voltage(args.voltage)
+            self.hx711_iface = SensorHX711Interface(self.logger, self.assembly,
+                                                    sck=args.sck, din=args.din, osc=args.osc)
 
-        self.mux_interface = iface = target.multiplexer.claim_interface(self, args)
-        iface.add_subtarget(SensorHX711Subtarget(
-            ports=iface.get_port_group(
-                clk=args.clk,
-                din=args.din,
-                osc=args.osc,
-            ),
-            out_fifo=iface.get_out_fifo(),
-            in_fifo=iface.get_in_fifo(),
-            # The highest conversion rate supported by this sensor is about 144 Hz, and at 1 MHz,
-            # the lowest retrieval rate is 37 kHz, so it'll always be fast enough unless the FIFO
-            # gets full.
-            clk_cyc=int(1e-6 * target.sys_clk_freq),
-            osc_cyc=osc_cyc,
-        ))
+    @classmethod
+    def add_setup_arguments(cls, parser):
+        parser.add_argument(
+            "-f", "--frequency", metavar="FREQ", type=float,
+            help="set reference oscillator frequency to FREQ MHz")
+
+    async def setup(self, args):
+        # The highest conversion rate supported by this sensor is about 144 Hz, and at 1 MHz,
+        # the lowest retrieval rate is 37 kHz, so it'll always be fast enough unless the FIFO
+        # gets full.
+        await self.hx711_iface.sck_clock.set_frequency(1e6)
+        if args.frequency is not None:
+            await self.hx711_iface.osc_clock.set_frequency(args.frequency * 1e6)
+        elif args.osc is not None:
+            raise HX711Error("osc pin assigned, but --frequency argument is missing.")
 
     async def run(self, device, args):
         iface = await device.demultiplexer.claim_interface(self, self.mux_interface, args)
-        return HX711Interface(iface, self.logger)
+        return SensorHX711Interface(iface, self.logger)
 
     @classmethod
-    def add_interact_arguments(cls, parser):
+    def add_run_arguments(cls, parser):
         parser.add_argument(
             "-c", "--channel", metavar="CHAN", type=str, choices=("A", "B"), default="A",
             help="measure channel CHAN")
@@ -195,17 +361,17 @@ class SensorHX711Applet(GlasgowApplet):
             "log", help="log measured values")
         DataLogger.add_subparsers(p_log)
 
-    async def interact(self, device, args, hx711):
-        await hx711.set_channel_gain(args.channel, args.gain)
+    async def run(self, args):
+        await self.hx711_iface.set_channel_gain(args.channel, args.gain)
 
         if args.operation == "measure":
-            sample = await hx711.sample()
+            sample = await self.hx711_iface.sample()
             print(f"count : {sample:+d} LSB")
 
         if args.operation == "log":
             data_logger = await DataLogger(self.logger, args, field_names={"n": "count(LSB)"})
             while True:
-                sample = await hx711.sample()
+                sample = await self.hx711_iface.sample()
                 await data_logger.report_data(fields={"n": sample})
 
     @classmethod
