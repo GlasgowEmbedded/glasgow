0482110e1cc1812831221f1c092bf531861a27ac
Catherine <whitequark@whitequark.org>
2026-02-11 16:20:43 +0000

    applet.display.pdi: remove.
    This is a V1 applet using the deprecated SPI controller, and it would
    require a non-trivial migration effort. I still have the device against
    which it was written, but given how obscure this early EPD is, it feels
    unlikely that this applet serves anyone's needs.
    
    We do not often remove applets but this one seems like a good choice
    for removal.

--- software/glasgow/applet/display/pdi/test.py
+++
@@ -1,8 +0,0 @@
-from ... import *
-from . import DisplayPDIApplet
-
-
-class DisplayPDIAppletTestCase(GlasgowAppletTestCase, applet=DisplayPDIApplet):
-    @synthesis_test
-    def test_build(self):
-        self.assertBuilds()
