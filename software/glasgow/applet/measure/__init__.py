88626fd7704a2d6ef45c8323dae475c59d56db9b
i-infra <77526490+i-infra@users.noreply.github.com>
2026-05-04 21:15:38 -0400

    applet.measure: ruff fix for measure taxon

--- software/glasgow/applet/measure/__init__.py
+++ software/glasgow/applet/measure/__init__.py
@@ -1,1 +1,1 @@
-"""The ``measure`` taxon groups applets that aid in the measurement of signals or the properties of devices."""
+"""The ``measure`` applets aid in the measurement of signals or the properties of devices."""
