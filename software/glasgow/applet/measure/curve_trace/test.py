a627726e8999d07bbf07f03757f3a1e4ec0c2354
infra <77526490+i-infra@users.noreply.github.com>
2026-05-31 11:14:06 -0400

    applet.measure.curve_trace: Delete null assert_builds

--- software/glasgow/applet/measure/curve_trace/test.py
+++
@@ -1,8 +0,0 @@
-from glasgow.applet import GlasgowAppletV2TestCase, synthesis_test
-from . import CurveTraceApplet
-
-
-class CurveTraceAppletTestCase(GlasgowAppletV2TestCase, applet=CurveTraceApplet):
-    @synthesis_test
-    def test_build(self):
-        self.assertBuilds(["--port", "A", "-V", "3.3"])
