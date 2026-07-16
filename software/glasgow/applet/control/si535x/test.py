dd3b9d89c4707dc7de3f8f2850c1815c80d7416a
Florian Klink <flokli@flokli.de>
2026-07-16 19:00:42 +0200

    si535x: migrate test importlib_resources to files API
    The old method has been deprecated for a while, and seems to have
    stopped working with 7.x.
    
    This has been verified to work with both 6.x and 7.x.

--- software/glasgow/applet/control/si535x/test.py
+++ software/glasgow/applet/control/si535x/test.py
@@ -1,4 +1,4 @@
-import importlib_resources
+from importlib_resources import files
 
 from glasgow.applet import GlasgowAppletV2TestCase, synthesis_test, applet_v2_hardware_test
 from . import ControlSi535xApplet
@@ -11,7 +11,9 @@ class ControlSi535xAppletTestCase(GlasgowAppletV2TestCase, applet=ControlSi535xA
 
     @applet_v2_hardware_test(mocks=["si535x_iface._i2c_iface"], args=["-V", "3.3"])
     async def test_si5351a(self, applet: ControlSi535xApplet):
-        with importlib_resources.open_text(__name__, "fixtures/si5351a-registers.txt") as file:
-            await applet.si535x_iface.configure_si5351(
-                sequence=applet.si535x_iface.parse_file(file),
-                enable=0x01)
+        register_file = (
+            files(__name__).joinpath("fixtures/si5351a-registers.txt").open()
+        )
+        await applet.si535x_iface.configure_si5351(
+            sequence=applet.si535x_iface.parse_file(register_file), enable=0x01
+        )
