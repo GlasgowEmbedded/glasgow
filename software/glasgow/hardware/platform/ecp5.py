737ae99f78d82b835eb047f4003b5a792c4f312b
Catherine <whitequark@whitequark.org>
2026-07-22 04:11:10 +0000

    gateware.pll_deprecated: remove.

--- software/glasgow/hardware/platform/ecp5.py
+++ software/glasgow/hardware/platform/ecp5.py
@@ -10,6 +10,3 @@ __all__ = ["GlasgowECP5Platform"]
 class GlasgowECP5Platform(GlasgowPlatform, LatticeECP5Platform):
     def bitstream_filename(self, design_name):
         return f"{design_name}.bit"
-
-    def get_pll(self, pll):
-        raise NotImplementedError("get_pll() not implemented for ECP5")
