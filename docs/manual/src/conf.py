551298f2475a322a510264940d9a40b8d6eadc0b
Catherine <whitequark@whitequark.org>
2026-07-29 14:03:22 +0000

    manual: use correct ISO 639 codes.

--- docs/manual/src/conf.py
+++ docs/manual/src/conf.py
@@ -10,7 +10,7 @@ match language:
         project = "Glasgow Interface\u00a0Explorer"
         copyright = "2020—%Y, Glasgow Interface Explorer contributors"
         html_baseurl = "https://glasgow-embedded.org/latest/"
-    case "cn":
+    case "zh":
         project = "Glasgow 可重构数字接口调试器"
         copyright = "2020—%Y, Glasgow Interface Explorer contributors"
         html_baseurl = "https://glasgow-embedded.cn/"
