e02455d2324633c00972ea4d525aae775fb2b232
Catherine <whitequark@whitequark.org>
2026-07-30 05:35:44 +0000

    gateware.stream: fix `stream_get()` for `always_ready=True` streams.

--- software/glasgow/gateware/stream.py
+++ software/glasgow/gateware/stream.py
@@ -17,9 +17,11 @@ async def stream_put(ctx, stream, payload):
 
 
 async def stream_get(ctx, stream):
-    ctx.set(stream.ready, 1)
+    if not isinstance(stream.ready, Const):
+        ctx.set(stream.ready, 1)
     payload, = await ctx.tick().sample(stream.payload).until(stream.valid)
-    ctx.set(stream.ready, 0)
+    if not isinstance(stream.ready, Const):
+        ctx.set(stream.ready, 0)
     return payload
 
 
